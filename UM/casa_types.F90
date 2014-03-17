99f212e099f420aa1aa0730b7e272e6dffa3e0d1
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2014-03-17 04:17:52 +0000

    Ticket #47 - license URL in header edited
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@1453 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- UM/casa_types.F90
+++ UM/casa_types.F90
@@ -5,7 +5,7 @@
 ! (the "Licence").
 ! You may not use this file except in compliance with the Licence.
 ! A copy of the Licence and registration form can be obtained from
-! http://www.accessimulator.org.au/cable
+! http://www.cawcr.gov.au/projects/access/cable
 ! You need to register and read the Licence agreement before use.
 ! Please contact cable_help@nf.nci.org.au for any questions on
 ! registration and the Licence.
