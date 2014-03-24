11553e777e00b6904d973eb1d082b1953b586bac
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2014-03-24 00:46:25 +0000

    moved call for MPI_Bcast as per Ticket #23
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@1519 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_mpimaster.F90
+++ offline/cable_mpimaster.F90
@@ -363,6 +363,11 @@ SUBROUTINE mpidrv_master (comm)
    CALL master_cable_params(comm, met,air,ssnow,veg,bgc,soil,canopy,&
    &                         rough,rad,sum_flux,bal)
 
+   ! MPI: mvtype and mstype send out here instead of inside master_casa_params
+   !      so that old CABLE carbon module can use them. (BP May 2013)
+   CALL MPI_Bcast (mvtype, 1, MPI_INTEGER, 0, comm, ierr)
+   CALL MPI_Bcast (mstype, 1, MPI_INTEGER, 0, comm, ierr)
+
    ! MPI: casa parameters scattered only if cnp module is active
    IF (icycle>0) THEN
      ! MPI:
@@ -2275,8 +2280,9 @@ SUBROUTINE master_casa_params (comm,casabiome,casapool,casaflux,casamet,&
 
   INTEGER :: rank, off, cnt
 
-  CALL MPI_Bcast (mvtype, 1, MPI_INTEGER, 0, comm, ierr)
-  CALL MPI_Bcast (mstype, 1, MPI_INTEGER, 0, comm, ierr)
+!  moved to calling before this subroutine (BP May 2013)
+!  CALL MPI_Bcast (mvtype, 1, MPI_INTEGER, 0, comm, ierr)
+!  CALL MPI_Bcast (mstype, 1, MPI_INTEGER, 0, comm, ierr)
 
   ntyp = ncasaparam
 
