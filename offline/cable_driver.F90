c9ee90d3cf5699280ed6984e776648250d82c32a
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2014-08-27 23:05:11 +0000

    ''Trunk Update'' '''Type:''' New feature: technical '''Ticket:''' #53
    __Overview__
    A runtime checking mechanism (at least for offline/serial apps anyway), to verify that technical changes and/or science changes behind switches that when they are turned off do NOT change the model output.
    
    Please Note: This tool is restricted to serial, offline applications. The ticket will be re-opened or a new ticket generated to implement in the UM and/or for multiple processors.
    
    __Technical comments__
    _driver sums balances and writes to a hidden file. This summed balance is compared within the model to the summed balance written to a hidden file generated by running an earlier, standard revision. Typically this standard revision will be the point on the trunk from which you branched.
    
    See Ticket #53 for further details on usage.
    
    __Science comments__
    neutral
    
    
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@2285 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_driver.F90
+++ offline/cable_driver.F90
@@ -186,6 +186,18 @@ PROGRAM cable_offline_driver
                   satuParam,        &
                   cable_user           ! additional USER switches 
 
+   ! Vars for standard for quasi-bitwise reproducability b/n runs
+   ! Check triggered by cable_user%consistency_check = .TRUE. in cable.nml
+   CHARACTER(len=30), PARAMETER ::                                             &
+      Ftrunk_sumbal  = ".trunk_sumbal",                                        &
+      Fnew_sumbal    = "new_sumbal"
+
+   DOUBLE PRECISION ::                                                                     &
+      trunk_sumbal = 0.0, & !
+      new_sumbal = 0.0
+
+   INTEGER :: ioerror
+
    ! END header
 
    ! Open, read and close the namelist file.
@@ -193,6 +205,16 @@ PROGRAM cable_offline_driver
       READ( 10, NML=CABLE )   !where NML=CABLE defined above
    CLOSE(10)
 
+   ! Open, read and close the consistency check file.
+   ! Check triggered by cable_user%consistency_check = .TRUE. in cable.nml
+   IF(cable_user%consistency_check) THEN 
+      OPEN( 11, FILE = Ftrunk_sumbal,STATUS='old',ACTION='READ',IOSTAT=ioerror )
+         IF(ioerror==0) then
+            READ( 11, * ) trunk_sumbal  ! written by previous trunk version
+         ENDIF
+      CLOSE(11)
+   ENDIF
+   
    ! Open log file:
    OPEN(logn,FILE=filename%log)
  
@@ -435,6 +457,35 @@ PROGRAM cable_offline_driver
                            sum_flux, veg )
 
    WRITE(logn,*) bal%wbal_tot, bal%ebal_tot, bal%ebal_tot_cncheck
+   
+   ! Check this run against standard for quasi-bitwise reproducability
+   ! Check triggered by cable_user%consistency_check = .TRUE. in cable.nml
+   IF(cable_user%consistency_check) THEN 
+      
+      new_sumbal = SUM(bal%wbal_tot) + SUM(bal%ebal_tot)                       &
+                       + SUM(bal%ebal_tot_cncheck)
+  
+      IF( new_sumbal == trunk_sumbal) THEN
+
+         print *, ""
+         print *, &
+         "Internal check shows this version reproduces the trunk sumbal"
+      
+      ELSE
+
+         print *, ""
+         print *, &
+         "Internal check shows in this version new_sumbal != trunk sumbal"
+         print *, &
+         "Writing new_sumbal to the file:", TRIM(Fnew_sumbal)
+               
+         OPEN( 12, FILE = Fnew_sumbal )
+            WRITE( 12, * ) new_sumbal  ! written by previous trunk version
+         CLOSE(12)
+      
+      ENDIF   
+      
+   ENDIF
 
    ! Close log file
    CLOSE(logn)
