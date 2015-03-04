2496e700aff0cc3ac5baf8f84f4569d105ff28c8
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2015-03-04 05:36:00 +0000

    ''Trunk Update'' '''Type:''' Technical code improvement '''Ticket:''' #63
    __Overview__
    Ticket #63 replaces duplicate functions psis/psis1 with psis only
    __Technical comments__
    remove duplicate function declarations and fix CALL accordingly
    __Science comments__
    neutral
    
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@2721 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- core/biogeophys/cable_canopy.F90
+++ core/biogeophys/cable_canopy.F90
@@ -497,9 +497,9 @@ SUBROUTINE define_canopy(bal,rad,rough,air,met,dels,ssnow,soil,veg, canopy)
             r_sc(j) = rough%rt0us(j) + rough%rt1usa(j) + rough%rt1usb(j) +     &
                       ( LOG( (zscl(j) - rough%disp(j)) /                       &
                       MAX( rough%zruffs(j)-rough%disp(j),                      &
-                      rough%z0soilsn(j) ) ) - psis1( (zscl(j)-rough%disp(j))   &
+                      rough%z0soilsn(j) ) ) - psis( (zscl(j)-rough%disp(j))    &
                       / (rough%zref_tq(j)/canopy%zetar(j,iterplus) ) )         &
-                      + psis1( (rough%zruffs(j) - rough%disp(j) )              &
+                      + psis( (rough%zruffs(j) - rough%disp(j) )               &
                       / (rough%zref_tq(j)/canopy%zetar(j,iterplus ) ) ) )      &
                       / C%VONK
 
@@ -999,45 +999,6 @@ ELEMENTAL FUNCTION psis(zeta) RESULT(r)
    r   = z*stable + (1.0-z)*unstable
 
 END FUNCTION psis
-
-! -----------------------------------------------------------------------------
-
-FUNCTION psis1(zeta) RESULT(r)
-   ! mrr, 16-sep-92 (from function psi: mrr, edinburgh 1977)
-   ! computes integrated stability function psis(z/l) (z/l=zeta)
-   ! for scalars, using the businger-dyer form for unstable cases
-   ! and the webb form for stable cases. see paulson (1970).
-   REAL, INTENT(IN)     :: zeta
-   
-   REAL, PARAMETER      :: gu = 16.0
-   REAL, PARAMETER      :: gs = 5.0
-   REAL, PARAMETER      :: a = 1.0
-   REAL, PARAMETER      :: b = 0.667
-   REAL, PARAMETER      :: c = 5.0
-   REAL, PARAMETER      :: d = 0.35
- 
-   REAL                 :: r
-   REAL                 :: stable
-   REAL                 :: unstable
-   REAL                 :: stzeta
- 
-   REAL                 :: z
-   REAL                 :: y
-   !REAL                 :: stable
-   !REAL                 :: unstable
- 
-   z      = 0.5 + sign(0.5,zeta)    ! z=1 in stable, 0 in unstable 
-   
-   ! Beljaars and Holtslag (1991) for stable
-   stzeta = max(0.,zeta)
-   stable = -(1.+2./3.*a*stzeta)**(3./2.) -  &
-             b*(stzeta-c/d)*exp(-d*stzeta) - b*c/d + 1.
- 
-   y      = (1.0 + gu*abs(zeta))**0.5
-   unstable = 2.0 * alog((1+y)*0.5)
-   r   = z*stable + (1.0-z)*unstable
-
-END FUNCTION psis1
 
 ! -----------------------------------------------------------------------------
 
