6dab3f563273729eb9eb944715fa4e1c98cafaf4
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2016-04-13 04:32:43 +0000

    ''Trunk Update'' '''Type:''' Technical correction. '''Ticket:''' #98
    __Overview__
    CABLE types defined in cable_define_types.F90 were accessed in cable_explicit_driver through a USE statement. Unecesary duplication was removed. 
    
    __Technical comments__
    CABLE types defined in cable_define_types.F90 were intended to be accessed in cable_explicit_driver through a USE statement. These "types" are instantiated in cable_um_tech where they are accessed from cable_def_types. In cable_explicit_driver the CABLE types were inherited by USEing them from cable_um_tech, and also cable_def_types, where they were not actually instantiated. Earlier versions of Intel compilers lethis go and seeming inherited the declared types which were actually there. However, ifort version 16 does not let this slide and so the unecesary and incorrect duplication was removed. 
    __Science comments__
    Makes reproducability, compiling capable with version 16 of Intel fortran,
    
    
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@3560 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- UM/cable_explicit_driver.F90
+++ UM/cable_explicit_driver.F90
@@ -67,8 +67,7 @@ SUBROUTINE cable_explicit_driver( row_length, rows, land_pts, ntiles,npft,     &
    !--- subr to call CABLE model
    USE cable_cbm_module, ONLY : cbm
 
-   USE cable_def_types_mod, ONLY : mp, ms, ssnow, rough, canopy, air, rad,     &
-                                   met
+   USE cable_def_types_mod, ONLY : mp, ms
 
    !--- include subr called to write data for testing purposes 
    USE cable_diag_module
