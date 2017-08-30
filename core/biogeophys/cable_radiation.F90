866e54771d220d70f8b9fe0c2a86f4804c23d1d9
inh599 <inh599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2017-08-30 02:43:54 +0000

    grand merge 1: cls package and early work for rev corr package
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/inh599/CMIP6-GM@4462 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- core/biogeophys/cable_radiation.F90
+++ core/biogeophys/cable_radiation.F90
@@ -108,7 +108,8 @@ SUBROUTINE init_radiation( met, rad, veg, canopy )
    ! Canopy REFLection of diffuse radiation for black leaves:
    DO ictr=1,nrb
 
-     rad%rhocdf(:,ictr) = rhoch(:,ictr) *                                      &
+     !INH: implementation of a VH bug
+     rad%rhocdf(:,ictr) = rhoch(:,ictr) * 2. *                                 &
                           ( C%GAUSS_W(1) * xk(:,1) / ( xk(:,1) + rad%extkd(:) )&
                           + C%GAUSS_W(2) * xk(:,2) / ( xk(:,2) + rad%extkd(:) )&
                           + C%GAUSS_W(3) * xk(:,3) / ( xk(:,3) + rad%extkd(:) ) )
