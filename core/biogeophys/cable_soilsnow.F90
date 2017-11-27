c28f126bdb3014915c7e4b7b9a3c87083645ff9e
mrd561 <mrd561@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2017-11-27 03:07:37 +0000

    remove lines from snow density that are not needed and cause compiler issues
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/mrd561/CMIP6-GM2_dev_testing@4934 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- core/biogeophys/cable_soilsnow.F90
+++ core/biogeophys/cable_soilsnow.F90
@@ -555,15 +555,10 @@ SUBROUTINE snowdensity (dels, ssnow, soil)
 
    TYPE(soil_parameter_type), INTENT(INOUT) :: soil
 
-   INTEGER, DIMENSION(mp,3) :: ssnow_isflag_ssdn
    REAL, DIMENSION(mp) :: ssnow_tgg_min1
-   REAL, DIMENSION(mp,3) :: dels_ssdn, ssnow_tgg_min
 
    CALL point2constants( C )
 
-   ssnow_isflag_ssdn = SPREAD( ssnow%isflag,2,mp)
-
-   dels_ssdn = SPREAD( SPREAD( dels, 1, mp ), 2,  mp )
    ssnow_tgg_min1 = MIN( C%TFRZ, ssnow%tgg(:,1) )
 
    WHERE( ssnow%snowd > 0.1 .AND. ssnow%isflag == 0 )
