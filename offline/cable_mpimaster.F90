4e07a25847abc2d6331d1f7efc778e04ec41b692
mgk576 <mgk576@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2019-07-23 09:57:30 +0000

    Added output vars to mpi
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/mgk576/trunk_DESICA_PFTs@6169 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_mpimaster.F90
+++ offline/cable_mpimaster.F90
@@ -3207,13 +3207,6 @@ CONTAINS
        CALL MPI_Get_address (ssnow%weighted_psi_soil(off), displs(bidx), ierr)
        blen(bidx) = r1len
 
-       !  plant hydraulics, mgdk576, 23/07/2019
-       CALL MPI_Get_address (ssnow%psi_soil(off,1), maddr(midx), ierr) ! 12
-       CALL MPI_Type_create_hvector (ms, r2len, r2stride, MPI_BYTE, &
-            &                        mat_t(midx, rank), ierr)
-       CALL MPI_Type_commit (mat_t(midx, rank), ierr)
-       midx = midx + 1
-
        bidx = bidx + 1
        CALL MPI_Get_address (veg%vlaimax(off), displs(bidx), ierr)
        blen(bidx) = r1len
@@ -4986,6 +4979,15 @@ CONTAINS
             &                        mat_t(midx, rank), ierr)
        CALL MPI_Type_commit (mat_t(midx, rank), ierr)
        midx = midx + 1
+
+       !  plant hydraulics, mgdk576, 23/07/2019
+       ! REAL(r_1)
+       CALL MPI_Get_address (ssnow%psi_soil(off,1), maddr(midx), ierr) ! 12
+       CALL MPI_Type_create_hvector (ms, r1len, r1stride, MPI_BYTE, &
+            &                        mat_t(midx, rank), ierr)
+       CALL MPI_Type_commit (mat_t(midx, rank), ierr)
+       midx = midx + 1
+
        ! REAL(r_1)
        CALL MPI_Get_address (ssnow%evapfbl(off,1), maddr(midx), ierr) ! 12
        CALL MPI_Type_create_hvector (ms, r1len, r1stride, MPI_BYTE, &
