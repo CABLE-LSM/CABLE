8a022362e5877106dbf6e5a89255f489f48990d3
mgk576 <mgk576@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2019-09-03 10:57:31 +0000

    Commented out duplicate calls that shouldn't be in trunk
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/mgk576/trunk_DESICA_PFTs@6306 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_mpimaster.F90
+++ offline/cable_mpimaster.F90
@@ -3326,9 +3326,10 @@ CONTAINS
        CALL MPI_Get_address (soil%slope_std(off), displs(bidx), ierr)
        blen(bidx) = r2len
 
-       bidx = bidx + 1
-       CALL MPI_Get_address (soil%GWdz(off), displs(bidx), ierr)
-       blen(bidx) = r2len
+       ! duplicate call - should be deleted, mgk576
+       !bidx = bidx + 1
+       !CALL MPI_Get_address (soil%GWdz(off), displs(bidx), ierr)
+       !blen(bidx) = r2len
 
        bidx = bidx + 1
        CALL MPI_Get_address (ssnow%GWwb(off), displs(bidx), ierr)
