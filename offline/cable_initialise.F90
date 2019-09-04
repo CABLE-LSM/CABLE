06d8346ff18ba91df2cc1e7f08769b6fda182614
mgk576 <mgk576@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2019-09-04 14:05:28 +0000

    Added missing init for weighted_psi_soil
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/mgk576/trunk_DESICA_PFTs@6308 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_initialise.F90
+++ offline/cable_initialise.F90
@@ -148,6 +148,7 @@ CONTAINS
     canopy%psi_stem_prev = -0.5
     canopy%psi_soil_prev = -0.001
     ssnow%tot_bg_resist = 1E9
+    ssnow%weighted_psi_soil = -0.1
 
 
   END SUBROUTINE get_default_inits
