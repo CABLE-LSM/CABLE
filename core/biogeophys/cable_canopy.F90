7c5ea1f9706d8addfd251ac65cadc5066b43b360
mgk576 <mgk576@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2019-09-20 06:41:07 +0000

    Fixed Nan catch
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Users/mgk576/trunk_DESICA_PFTs@6413 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- core/biogeophys/cable_canopy.F90
+++ core/biogeophys/cable_canopy.F90
@@ -386,7 +386,6 @@ CONTAINS
        rny = SUM(rad%rniso,2) ! init current estimate net rad
        hcy = 0.0              ! init current estimate lat heat
        ecy = rny - hcy        ! init current estimate lat heat
-
        sum_rad_rniso = SUM(rad%rniso,2)
 
        CALL dryLeaf( dels, rad, rough, air, met,                                &
@@ -2285,9 +2284,9 @@ CONTAINS
                             soil%swilt(i) ) * soil%zse(kk) * 1000.0 )
 
                    ENDDO
+                   canopy%fevc(i) = SUM(ssnow%evapfbl(i,:))*air%rlam(i)/dels
+                   ecx(i) = canopy%fevc(i) / (1.0-canopy%fwet(i))
                ENDIF
-               canopy%fevc(i) = SUM(ssnow%evapfbl(i,:))*air%rlam(i)/dels
-               ecx(i) = canopy%fevc(i) / (1.0-canopy%fwet(i))
 
                !canopy%fevc(i) = ecx(i)*(1.0-canopy%fwet(i))
                !
@@ -2406,15 +2405,9 @@ CONTAINS
              rny(i) = rnx(i)
              hcy(i) = hcx(i)
              ecy(i) = ecx(i)
-
-             ! Catch NaN issue for big jumps, not sure of the cause?
-             ! mgk576
-             if (ecy(i)  /=ecy(i) ) then
-                ecy(i) = 0.0
-             endif
-
              rdy(i,:) = rdx(i,:)
              an_y(i,:) = anx(i,:)
+
              ! save last values calculated for ssnow%evapfbl
              oldevapfbl(i,:) = ssnow%evapfbl(i,:)
 
@@ -2428,8 +2421,8 @@ CONTAINS
     ! dry canopy flux
     canopy%fevc = (1.0-canopy%fwet) * ecy
 
-    !IF (cable_user%fwsoil_switch.NE.'Haverd2013' .AND. cable_user%fwsoil_switch.NE.'hydraulics') THEN
-    IF (cable_user%fwsoil_switch.NE.'Haverd2013') THEN
+    IF (cable_user%fwsoil_switch.NE.'Haverd2013' .AND. cable_user%fwsoil_switch.NE.'hydraulics') THEN
+    !IF (cable_user%fwsoil_switch.NE.'Haverd2013') THEN
 
        ! Recalculate ssnow%evapfbl as ecy may not be updated with the ecx
        ! calculated in the last iteration.
