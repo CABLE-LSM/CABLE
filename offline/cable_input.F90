99fa9d5fa4a86f364832d03b468a961e31559c66
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2020-06-16 06:48:31 +0000

    ''Trunk Update''
    '''Type:'''
    Fix previous merge that was inadvertently removed
    
    '''Ticket:'''
    #212
    
    __Overview__
    originally included in push to trunk@5929
    
    
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@7243 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- offline/cable_input.F90
+++ offline/cable_input.F90
@@ -1003,6 +1003,7 @@ CONTAINS
     ! Look for Rainf (essential):- - - - - - - - - - - - - - - - - -
     IF (ncciy > 0) ncid_met = ncid_rain
     ok = NF90_INQ_VARID(ncid_met,'Rainf',id%Rainf)
+    IF(ok .NE. NF90_NOERR) ok = NF90_INQ_VARID(ncid_met,'Precip',id%Rainf)
     IF(ok /= NF90_NOERR) CALL nc_abort &
          (ok,'Error finding Rainf in met data file ' &
          //TRIM(filename%met)//' (SUBROUTINE open_met_file)')
@@ -1089,6 +1090,7 @@ CONTAINS
     ! Look for PSurf (can be synthesised):- - - - - - - - - - - - - - - -
     IF (ncciy > 0) ncid_met = ncid_ps
     ok = NF90_INQ_VARID(ncid_met,'PSurf',id%PSurf)
+    IF(ok .NE. NF90_NOERR) ok = NF90_INQ_VARID(ncid_met,'Psurf',id%PSurf)
     IF(ok == NF90_NOERR) THEN ! If inquiry is okay
        exists%PSurf = .TRUE. ! PSurf is present in met file
        ! Get PSurf units and check:
