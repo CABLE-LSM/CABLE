f68b5b8abde95300443ce91f6c08164830b7424d
jxs599 <jxs599@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2021-09-20 00:34:51 +0000

    ''Trunk Update''
    '''Type:'''
    trivial syntax and formatting
    
    '''Ticket:'''
    304
    
    __Overview__
    rm SAVE attribute as NOT appropriate for NGMS,JAC and does not appear to matter
    
    
    git-svn-id: https://trac.nci.org.au/svn/cable/trunk@8398 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- util/cable_common.F90
+++ util/cable_common.F90
@@ -1,5 +1,5 @@
 !==============================================================================
-! This source code is part of the
+! This source code is part of the 
 ! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
 ! This work is licensed under the CSIRO Open Source Software License
 ! Agreement (variation of the BSD / MIT License).
@@ -27,62 +27,62 @@ USE cable_runtime_opts_mod ,ONLY : cable_user
 USE cable_runtime_opts_mod ,ONLY : satuparam
 USE cable_runtime_opts_mod ,ONLY : wiltparam
 
-  IMPLICIT NONE
+   IMPLICIT NONE 
 
-  !---allows reference to "gl"obal timestep in run (from atm_step)
-  !---total number of timesteps, and processing node
-  INTEGER, SAVE :: ktau_gl, kend_gl, knode_gl, kwidth_gl
-
+   !---allows reference to "gl"obal timestep in run (from atm_step)
+   !---total number of timesteps, and processing node 
+   INTEGER, SAVE :: ktau_gl, kend_gl, knode_gl, kwidth_gl
+   
   LOGICAL :: L_fudge = .FALSE.
 
   INTEGER, SAVE :: CurYear  ! current year of multiannual run
 
-  ! set from environment variable $HOME
-  CHARACTER(LEN=200) ::                                                       &
-       myhome
+   ! set from environment variable $HOME
+   CHARACTER(LEN=200) ::                                                       & 
+      myhome
 
   ! switch to calc sil albedo using soil colour - Ticket #27
   LOGICAL :: calcsoilalbedo = .FALSE.
-  !---Lestevens Sept2012
-  !---CASACNP switches and cycle index
-  LOGICAL, SAVE :: l_casacnp,l_laiFeedbk,l_vcmaxFeedbk
+   !---Lestevens Sept2012
+   !---CASACNP switches and cycle index
+   LOGICAL, SAVE :: l_casacnp,l_laiFeedbk,l_vcmaxFeedbk
    LOGICAL :: l_luc = .FALSE.
    LOGICAL :: l_thinforest = .FALSE.
-
-  !---CABLE runtime switches def in this type
-  TYPE kbl_internal_switches
-     LOGICAL :: um = .FALSE., um_explicit = .FALSE., um_implicit = .FALSE.,   &
+   
+   !---CABLE runtime switches def in this type
+   TYPE kbl_internal_switches
+      LOGICAL :: um = .FALSE., um_explicit = .FALSE., um_implicit = .FALSE.,   &
           um_radiation = .FALSE., um_hydrology = .FALSE.
-     LOGICAL :: offline = .FALSE., mk3l = .FALSE.
-  END TYPE kbl_internal_switches
+      LOGICAL :: offline = .FALSE., mk3l = .FALSE.
+   END TYPE kbl_internal_switches 
 
   ! instantiate internal switches
-  TYPE(kbl_internal_switches), SAVE :: cable_runtime
+   TYPE(kbl_internal_switches), SAVE :: cable_runtime
 
-  ! external files read/written by CABLE
-  TYPE filenames_type
+   ! external files read/written by CABLE
+   TYPE filenames_type
 
      CHARACTER(LEN=500) ::                                                        &
-          met,        & ! name of file for CABLE input
+      met,        & ! name of file for CABLE input
           path='./',       & ! path for output and restart files for CABLE and CASA
-          out,        & ! name of file for CABLE output
-          log,        & ! name of file for execution log
+      out,        & ! name of file for CABLE output
+      log,        & ! name of file for execution log
           restart_in = ' ', & ! name of restart file to read
-          restart_out,& ! name of restart file to read
-          LAI,        & ! name of file for default LAI
-          TYPE,       & ! file for default veg/soil type
-          veg,        & ! file for vegetation parameters
-          soil,       & ! name of file for soil parameters
+      restart_out,& ! name of restart file to read
+      LAI,        & ! name of file for default LAI
+      type,       & ! file for default veg/soil type
+      veg,        & ! file for vegetation parameters
+      soil,       & ! name of file for soil parameters
           soilcolor,  & ! file for soil color(soilcolor_global_1x1.nc)
-          inits,      & ! name of file for initialisations
+      inits,      & ! name of file for initialisations
           soilIGBP,   & ! name of file for IGBP soil map
           gw_elev       !name of file for gw/elevation data
 
-  END TYPE filenames_type
+   END TYPE filenames_type
 
-  TYPE(filenames_type), SAVE :: filename
+   TYPE(filenames_type) :: filename
 
-  ! hydraulic_redistribution switch _soilsnow module
+   ! hydraulic_redistribution switch _soilsnow module
   LOGICAL :: redistrb = .FALSE.  
 
   TYPE organic_soil_params
@@ -96,7 +96,7 @@ USE cable_runtime_opts_mod ,ONLY : wiltparam
           watr_organic   = 0.1,     &
           sfc_vec_hk      = 1.157407e-06, &
           swilt_vec_hk      = 2.31481481e-8
-
+   
   END TYPE organic_soil_params
 
   TYPE gw_parameters_type
@@ -160,5 +160,6 @@ contains
   END FUNCTION LEAP_DAY
 
 
-
+      
 END MODULE cable_common_module
+
