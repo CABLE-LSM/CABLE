019cbdc1ff55624435a6a5cd18267559b219ddb4
aidanheerdegen <aidanheerdegen@users.noreply.github.com>
2022-10-26 06:27:36 +0000

    Deploy preview for PR 4 🛫

---
+++ pr-preview/pr-4/api/src/cable_params_mod.F90
@@ -0,0 +1,261 @@
+MODULE cable_params_mod
+!jhan: This is currently hard-wired as USEd module was NA in ESM1.5 and NOT ideal to be 
+! USEing any data, especially JULES da
+!USE max_dimensions,             ONLY: ntype_max ! defined PARAMETER @ compile time
+
+!H! Elevate these to namelist definable
+USE cable_other_constants_mod,  ONLY: nsl
+USE cable_other_constants_mod,  ONLY: nrb       !# radiation "bANDS" 
+                                                !dir/dif components in bands VIS/NIR
+USE cable_other_constants_mod,  ONLY: nscs      ! number of soil carbon stores
+USE cable_other_constants_mod,  ONLY: nvcs      ! number of vegetation carbon stores
+USE grid_constants_cbl_mod, ONLY : mstype => nsoiltypes  ! # of soil types [9]
+
+IMPLICIT NONE
+
+PUBLIC :: veg_parameter_type
+PUBLIC :: vegin_type
+PUBLIC :: soil_parameter_type
+PUBLIC :: soilin_type
+
+!jhan: eventuaLLY REMOVE THESE instances as PUBLIC
+PUBLIC :: veg_cbl
+PUBLIC :: vegin
+PUBLIC :: soil_cbl
+PUBLIC :: soilin
+
+INTEGER, PARAMETER :: ntype_max = 17 
+!-----------------------------------------------------------------------------
+! Description:
+!   Defines variable types and variables for CABLE standalone runs.
+!   Based on cable_def_types_mod.F90 from the CABLE trunk.
+!
+! Code Owner: Please refer to ModuleLeaders.txt
+! This file belongs in CABLE SCIENCE
+!-----------------------------------------------------------------------------
+
+! Vegetation parameters used:
+TYPE veg_parameter_type
+!jhan: surely %meth doesn't need to be 'mp' here
+     INTEGER, DIMENSION(:), POINTER ::                                        &
+          meth,    & ! method for calculation of canopy fluxes and temp.
+          iveg , &      ! vegetation type
+          iLU ! land use type
+     REAL, DIMENSION(:), POINTER ::                                           &
+          canst1,  & ! max intercepted water by canopy (mm/LAI)
+          dleaf,   & ! chararacteristc legnth of leaf (m)
+          ejmax,   & ! max pot. electron transp rate top leaf(mol/m2/s)
+          frac4,   & ! fraction of c4 plants
+          hc,      & ! roughness height of canopy (veg - snow)
+          vlai,    & ! leaf area index
+          xalbnir, &
+          rp20,    & ! plant respiration coefficient at 20 C
+          rpcoef,  & ! temperature coef nonleaf plant respiration (1/C)
+          rs20,    & ! soil respiration at 20 C [mol m-2 s-1]
+          shelrb,  & ! sheltering factor (dimensionless)
+          vegcf,   & ! kdcorbin, 08/10
+          tminvj,  & ! min temperature of the start of photosynthesis
+          toptvj,  & ! opt temperature of the start of photosynthesis
+          tmaxvj,  & ! max temperature of the start of photosynthesis
+          vbeta,   & !
+          vcmax,   & ! max RuBP carboxylation rate top leaf (mol/m2/s)
+          xfang,   & ! leaf angle PARAMETER
+          extkn,   & ! extinction coef for vertical
+          vlaimax, & ! extinction coef for vertical
+          wai,     & ! wood area index (stem+branches+twigs)
+          a1gs,    & ! a1 parameter in stomatal conductance model
+          d0gs,    & ! d0 in stomatal conductance model
+          alpha,   & ! initial slope of J-Q response curve
+          convex,  & ! convexity of J-Q response curve
+          cfrd,    & ! ratio of day respiration to vcmax
+          gswmin,  & ! minimal stomatal conductance
+          conkc0,  &  ! Michaelis-menton constant for carboxylase
+          conko0,  &  ! Michaelis-menton constant for oxygenase
+          ekc,     &  ! activation energy for caroxylagse
+          eko,     &  ! acvtivation enegery for oxygenase
+          g0,      & ! Belinda's stomatal model intercept, Ticket #56.
+          g1         ! Belinda's stomatal model slope, Ticket #56.
+
+     LOGICAL, DIMENSION(:), POINTER ::                                        &
+          deciduous ! flag used for phenology fix
+
+     REAL, DIMENSION(:,:), POINTER ::                                         &
+          refl,    &
+          taul,    &
+          froot      ! fraction of root in each soil layer
+
+     ! Additional  veg parameters:
+     REAL, DIMENSION(:), POINTER :: rootbeta ! parameter for estimating vertical root mass distribution (froot)
+     REAL, DIMENSION(:), POINTER :: gamma    ! parameter in root efficiency function (Lai and Katul 2000)
+     REAL, DIMENSION(:), POINTER :: ZR       ! maximum rooting depth (cm)
+     REAL, DIMENSION(:), POINTER :: F10      ! fraction of roots in top 10 cm
+
+     REAL, DIMENSION(:), POINTER :: clitt     !
+
+     ! Additional POP veg param
+     INTEGER, DIMENSION(:,:), POINTER ::  disturbance_interval
+     REAL, DIMENSION(:,:), POINTER ::  disturbance_intensity
+
+  REAL,  POINTER ::                                                                     &
+       soil(:,:),                                                  &
+       csoil(:,:),                                                  &
+       cplant(:,:),                                                 &
+       ratecs(:,:),                                                            &
+       ratecp(:,:)
+
+  END TYPE veg_parameter_type
+
+
+! Vegetation parameters I/O:
+TYPE vegin_type
+  REAL ::                                                                     &
+       canst1(ntype_max),                                                      &
+       dleaf(ntype_max),                                                       &
+       length(ntype_max),                                                      &
+       width(ntype_max),                                                       &
+       vcmax(ntype_max),                                                       &
+       ejmax(ntype_max),                                                       &
+       hc(ntype_max),                                                          &
+       xfang(ntype_max),                                                       &
+       rp20(ntype_max),                                                        &
+       rpcoef(ntype_max),                                                      &
+       rs20(ntype_max),                                                        &
+       wai(ntype_max),                                                         &
+       rootbeta(ntype_max),                                                    &
+       shelrb(ntype_max),                                                      &
+       vegcf(ntype_max),                                                       &
+       frac4(ntype_max),                                                       &
+       xalbnir(ntype_max),                                                     &
+       extkn(ntype_max),                                                       &
+       tminvj(ntype_max),                                                      &
+       tmaxvj(ntype_max),                                                      &
+       vbeta(ntype_max),                                                       &
+       a1gs(ntype_max),                                                        &
+       d0gs(ntype_max),                                                        &
+       alpha(ntype_max),                                                       &
+       convex(ntype_max),                                                      &
+       cfrd(ntype_max),                                                        &
+       gswmin(ntype_max),                                                      &
+       conkc0(ntype_max),                                                      &
+       conko0(ntype_max),                                                      &
+       ekc(ntype_max),                                                         &
+       eko(ntype_max),                                                         &
+       g0(ntype_max),                                                          &
+       g1(ntype_max),                                                          &
+       zr(ntype_max),                                                          &
+       clitt(ntype_max),                                                       &
+       froot(nsl,ntype_max),                                                   &
+       csoil(nscs,ntype_max),                                                  &
+       ratecs(nscs,ntype_max),                                                 &
+       cplant(nvcs,ntype_max),                                                 &
+       ratecp(nvcs,ntype_max),                                                 &
+       refl(nrb,ntype_max),                                                    &
+       taul(nrb,ntype_max)
+END TYPE vegin_type
+
+! Soil parameters used:
+  TYPE soil_parameter_type
+
+     INTEGER, DIMENSION(:), POINTER ::                                        &
+          isoilm     ! integer soil type
+
+     REAL, DIMENSION(:), POINTER ::                                           &
+          bch,     & ! parameter b in Campbell equation
+          c3,      & ! c3 drainage coeff (fraction)
+          clay,    & ! fraction of soil which is clay
+          css,     & ! soil specific heat capacity [kJ/kg/K]
+          hsbh,    & ! difsat * etasat (=hyds*abs(sucs)*bch)
+          hyds,    & ! hydraulic conductivity @ saturation [m/s], Ksat
+          i2bp3,   & ! par. one in K vis suction (=nint(bch)+2)
+          ibp2,    & ! par. two in K vis suction (fn of pbch)
+          rhosoil, & ! soil density [kg/m3]
+          sand,    & ! fraction of soil which is sand
+          sfc,     & ! vol H2O @ field capacity
+          silt,    & ! fraction of soil which is silt
+          ssat,    & ! vol H2O @ saturation
+          sucs,    & ! suction at saturation (m)
+          swilt,   & ! vol H2O @ wilting
+          zse,     & ! thickness of each soil layer (1=top) in m
+          zshh,    & ! distance between consecutive layer midpoints (m)
+                                ! vars intro for Ticket #27
+          soilcol, & ! keep color for all patches/tiles
+          albsoilf   ! soil reflectance
+
+     REAL, DIMENSION(:,:), POINTER :: &
+          zse_vec,css_vec,cnsd_vec
+
+     REAL, DIMENSION(:), POINTER ::                                      &
+          cnsd,    & ! thermal conductivity of dry soil [W/m/K]
+          pwb_min    ! working variable (swilt/ssat)**ibp2
+
+     REAL, DIMENSION(:,:), POINTER ::                                         &
+          albsoil    ! soil reflectance (2nd dim. BP 21Oct2009)
+     !mrd561
+     !MD parameters for GW module that vary with soil layer
+     REAL, DIMENSION(:,:), POINTER ::                                    &
+          sucs_vec, & !psi at saturation in [mm]
+          hyds_vec,  & !saturated hydraulic conductivity  [mm/s]
+          bch_vec, & !C and H B [none]
+          clay_vec,  & !fraction of soil that is clay [frac]
+          sand_vec,  & !fraction of soil that is sand [frac]
+          silt_vec,  & !fraction of soil that is silt [frac]
+          org_vec,   & !fration of soil made of organic soils [frac]
+          rhosoil_vec,& !soil density  [kg/m3]
+          ssat_vec, & !volumetric water content at saturation [mm3/mm3]
+          watr,   & !residual water content of the soil [mm3/mm3]
+          sfc_vec, & !field capcacity (hk = 1 mm/day)
+          swilt_vec     ! wilting point (hk = 0.02 mm/day)
+
+     REAL, DIMENSION(:), POINTER ::                                      &
+          drain_dens,&!  drainage density ( mean dist to rivers/streams )
+          elev, &  !elevation above sea level
+          elev_std, &  !elevation above sea level
+          slope,  &  !mean slope of grid cell
+          slope_std  !stddev of grid cell slope
+
+     !MD parameters for GW module for the aquifer
+     REAL, DIMENSION(:), POINTER ::                                       &
+          GWsucs_vec,  &  !head in the aquifer [mm]
+          GWhyds_vec,   &  !saturated hydraulic conductivity of the aquifer [mm/s]
+          GWbch_vec,  & !clapp and horn b of the aquifer   [none]
+          GWssat_vec,  & !saturated water content of the aquifer [mm3/mm3]
+          GWwatr,    & !residual water content of the aquifer [mm3/mm3]
+          GWz,       & !node depth of the aquifer    [m]
+          GWdz,      & !thickness of the aquifer   [m]
+          GWrhosoil_vec    !density of the aquifer substrate [kg/m3]
+
+     ! Additional SLI parameters
+     INTEGER,   DIMENSION(:),   POINTER :: nhorizons ! number of soil horizons
+     INTEGER,   DIMENSION(:,:), POINTER :: ishorizon ! horizon number 1:nhorizons
+     REAL, DIMENSION(:),   POINTER :: clitt     ! litter (tC/ha)
+     REAL, DIMENSION(:),   POINTER :: zeta      ! macropore parameter
+     REAL, DIMENSION(:),   POINTER :: fsatmax   ! variably saturated area parameter
+
+  END TYPE soil_parameter_type
+
+
+
+! Soil parameters I/O:
+TYPE soilin_type
+  REAL ::                                                                     &
+       silt(mstype),                                                        &
+       clay(mstype),                                                        &
+       sand(mstype),                                                        &
+       swilt(mstype),                                                       &
+       sfc(mstype),                                                         &
+       ssat(mstype),                                                        &
+       bch(mstype),                                                         &
+       hyds(mstype),                                                        &
+       sucs(mstype),                                                        &
+       rhosoil(mstype),                                                     &
+       css(mstype)
+END TYPE soilin_type
+
+!Instantiate types
+TYPE(vegin_type) :: vegin !read from namelist
+TYPE(soilin_type) :: soilin !read from namelist
+
+TYPE(soil_parameter_type) :: soil_cbl !used in CABLE
+TYPE(veg_parameter_type) :: veg_cbl  !used in CABLE
+
+END MODULE cable_params_mod
---
+++ pr-preview/pr-4/api/src/cable_um_tech.F90
@@ -0,0 +1,252 @@
+!==============================================================================
+! This source code is part of the 
+! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
+! This work is licensed under the CABLE Academic User Licence Agreement 
+! (the "Licence").
+! You may not use this file except in compliance with the Licence.
+! A copy of the Licence and registration form can be obtained from 
+! http://www.cawcr.gov.au/projects/access/cable
+! You need to register and read the Licence agreement before use.
+! Please contact cable_help@nf.nci.org.au for any questions on 
+! registration and the Licence.
+!
+! Unless required by applicable law or agreed to in writing, 
+! software distributed under the Licence is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the Licence for the specific language governing permissions and 
+! limitations under the Licence.
+! ==============================================================================
+!
+! Purpose: Routines to read CABLE namelist, check variables, allocate and 
+!          deallocate CABLE arrays
+!
+! Contact: Jhan.Srbinovsky@csiro.au
+!
+! History: Rewrite of code from v1.8 (ACCESS1.3)
+!          soil_snow_type now ssnow (instead of ssoil)
+!
+!
+! ==============================================================================
+
+MODULE cable_um_tech_mod
+   
+   USE cable_def_types_mod, ONLY : air_type, bgc_pool_type, met_type,         &
+                   balances_type, radiation_type, roughness_type, sum_flux_type
+   USE cable_def_types_mod, ONLY : soil_snow_type
+   USE cable_def_types_mod, ONLY : canopy_type
+   USE cable_params_mod,    ONLY : veg_parameter_type
+   USE cable_params_mod,    ONLY : soil_parameter_type
+
+
+   IMPLICIT NONE
+
+   TYPE(air_type), SAVE             :: air
+   TYPE(bgc_pool_type), SAVE        :: bgc
+   TYPE(met_type), SAVE             :: met
+   TYPE(balances_type), SAVE        :: bal
+   TYPE(radiation_type), SAVE       :: rad
+   TYPE(roughness_type), SAVE       :: rough
+   TYPE(sum_flux_type), SAVE        :: sum_flux
+   TYPE(canopy_type), SAVE          :: canopy
+   TYPE(soil_snow_type), SAVE       :: ssnow
+   TYPE(veg_parameter_type), SAVE   :: veg
+   TYPE(soil_parameter_type), SAVE  :: soil
+
+   TYPE derived_rad_bands    
+      REAL, ALLOCATABLE ::                                                     &
+         SW_DOWN_DIR (:,:), & ! Surface downward SW direct radiation (W/m2).
+         SW_DOWN_DIF(:,:), & ! Surface downward SW diffuse radiation (W/m2).
+         SW_DOWN_VIS(:,:), & ! Surface downward VIS radiation (W/m2).
+         SW_DOWN_NIR(:,:), & ! Surface downward NIR radiation (W/m2).
+         FBEAM(:,:,:)      ! Surface downward SW radiation (W/m2).
+   END TYPE derived_rad_bands
+   
+   TYPE um_dimensions 
+      INTEGER :: row_length, rows, land_pts, ntiles, npft,                     &
+                 sm_levels, timestep 
+      INTEGER, ALLOCATABLE, DIMENSION(:) :: tile_pts, land_index
+      INTEGER, ALLOCATABLE, DIMENSION(:,:) :: tile_index
+      REAL :: rho_water
+      REAL,ALLOCATABLE, DIMENSION(:,:) :: tile_frac
+      REAL,ALLOCATABLE, DIMENSION(:,:) :: latitude, longitude
+      LOGICAL,ALLOCATABLE, DIMENSION(:,:) :: l_tile_pts
+   ENDTYPE um_dimensions 
+
+   TYPE derived_veg_pars
+      INTEGER, DIMENSION(:,:), POINTER ::                                      &
+         ivegt(:,:),    & ! vegetation  types
+         isoilm(:,:)      ! soil types
+      REAL, DIMENSION(:,:), POINTER ::                                         &
+         htveg(:,:),    &
+         laift(:,:)       ! hruffmax(:.:)
+   END TYPE derived_veg_pars
+
+   INTERFACE check_nmlvar 
+      MODULE PROCEDURE check_chvar, check_intvar, check_lgvar
+   END INTERFACE check_nmlvar 
+ 
+      TYPE(derived_rad_bands), SAVE :: kblum_rad    
+      TYPE(derived_veg_pars),  SAVE :: kblum_veg    
+      TYPE(um_dimensions),     SAVE :: um1
+      
+      REAL,ALLOCATABLE, DIMENSION(:) :: conv_rain_prevstep, conv_snow_prevstep 
+
+CONTAINS
+
+!========================================================================
+!========================================================================
+!========================================================================
+
+SUBROUTINE cable_um_runtime_vars(runtime_vars_file) 
+   USE cable_common_module, ONLY : cable_runtime, cable_user, filename,        &
+                                   cable_user, knode_gl, redistrb, wiltParam,  &
+                                   satuParam, l_casacnp, l_laiFeedbk,          &
+                                   l_vcmaxFeedbk, l_luc, l_thinforest
+   USE casavariable, ONLY : casafile
+   USE casadimension, ONLY : icycle
+
+
+   CHARACTER(LEN=*), INTENT(IN) :: runtime_vars_file
+   INTEGER :: funit=88
+   
+   !--- namelist for CABLE runtime vars, files, switches 
+   NAMELIST/CABLE/filename, l_thinforest, l_luc, l_casacnp, l_laiFeedbk, &
+                  l_vcmaxFeedbk, icycle,   &
+                  casafile, cable_user, redistrb, wiltParam, satuParam
+
+      !--- assume namelist exists. no iostatus check 
+      OPEN(unit=funit,FILE= runtime_vars_file)
+         READ(funit,NML=CABLE)
+         IF( knode_gl==0)  THEN
+            PRINT *, '  '; PRINT *, 'CABLE_log:' 
+            PRINT *, '  Opened file - '
+            PRINT *, '  ', trim(runtime_vars_file)
+            PRINT *, '  for reading runtime vars.' 
+            PRINT *, 'End CABLE_log:'; PRINT *, '  '
+        ENDIF
+      CLOSE(funit)
+
+      if (knode_gl==0) then
+        print *, '  '; print *, 'CASA_log:'
+        print *, '  icycle =',icycle
+        print *, '  l_casacnp =',l_casacnp
+        print *, '  l_laiFeedbk =',l_laiFeedbk
+        print *, '  l_vcmaxFeedbk =',l_vcmaxFeedbk
+        print *, 'End CASA_log:'; print *, '  '
+      endif
+      IF (l_casacnp  .AND. (icycle == 0 .OR. icycle > 3)) &
+          STOP 'CASA_log: icycle must be 1 to 3 when using casaCNP'
+      IF ((.NOT. l_casacnp)  .AND. (icycle >= 1)) &
+          STOP 'CASA_log: icycle must be <=0 when not using casaCNP'
+      IF ((l_laiFeedbk .OR. l_vcmaxFeedbk) .AND. (.NOT. l_casacnp)) &
+          STOP 'CASA_log: casaCNP required to get prognostic LAI or Vcmax'
+      IF (l_vcmaxFeedbk .AND. icycle < 2) &
+          STOP 'CASA_log: icycle must be 2 to 3 to get prognostic Vcmax'
+   
+      !--- check value of variable 
+      CALL check_nmlvar('filename%veg', filename%veg)
+      CALL check_nmlvar('filename%soil', filename%soil)
+      CALL check_nmlvar('cable_user%DIAG_SOIL_RESP', cable_user%DIAG_SOIL_RESP)
+      CALL check_nmlvar('cable_user%LEAF_RESPIRATION',                         &
+                        cable_user%LEAF_RESPIRATION)
+      CALL check_nmlvar('cable_user%FWSOIL_SWITCH', cable_user%FWSOIL_SWITCH)
+      CALL check_nmlvar('cable_user%RUN_DIAG_LEVEL', cable_user%RUN_DIAG_LEVEL)
+      CALL check_nmlvar('cable_user%l_new_roughness_soil',                     &
+                         cable_user%l_new_roughness_soil)
+      CALL check_nmlvar('cable_user%l_new_roughness_soil',                     &
+                         cable_user%l_new_roughness_soil)
+      CALL check_nmlvar('cable_user%l_new_roughness_soil',                     &
+                         cable_user%l_new_roughness_soil)
+
+END SUBROUTINE cable_um_runtime_vars
+
+!jhan: also add real, logical, int interfaces
+SUBROUTINE check_chvar(this_var, val_var)
+   USE cable_common_module, ONLY : knode_gl
+
+   CHARACTER(LEN=*), INTENT(IN) :: this_var, val_var 
+   
+      IF (knode_gl==0) THEN
+         PRINT *, '  '; PRINT *, 'CABLE_log:' 
+         PRINT *, '   run time variable - '
+         PRINT *, '  ', trim(this_var) 
+         PRINT *, '   defined as - '
+         PRINT *, '  ', trim(val_var) 
+         PRINT *, 'End CABLE_log:'; PRINT *, '  '
+      ENDIf
+
+END SUBROUTINE check_chvar
+
+SUBROUTINE check_intvar(this_var, val_var)
+   USE cable_common_module, ONLY : knode_gl
+
+   CHARACTER(LEN=*), INTENT(IN) :: this_var
+   INTEGER, INTENT(IN) :: val_var 
+
+      IF (knode_gl==0) THEN
+         PRINT *, '  '; PRINT *, 'CABLE_log:' 
+         PRINT *, '   run time variable - '
+         PRINT *, '  ', trim(this_var) 
+         PRINT *, '   defined as - '
+         PRINT *, '  ', val_var
+         PRINT *, 'End CABLE_log:'; PRINT *, '  '
+      ENDIF
+
+END SUBROUTINE check_intvar
+
+SUBROUTINE check_lgvar(this_var, val_var)
+   USE cable_common_module, ONLY : knode_gl
+
+   CHARACTER(LEN=*), INTENT(IN) :: this_var
+   LOGICAL, INTENT(IN) :: val_var
+
+      IF (knode_gl==0) THEN
+         PRINT *, '  '; PRINT *, 'CABLE_log:'
+         PRINT *, '   run time variable - '
+         PRINT *, '  ', trim(this_var)
+         PRINT *, '   defined as - '
+         PRINT *, '  ', (val_var)
+         PRINT *, 'End CABLE_log:'; PRINT *, '  '
+      ENDIf
+
+END SUBROUTINE check_lgvar
+    
+!========================================================================= 
+!=========================================================================
+!========================================================================= 
+ 
+SUBROUTINE alloc_um_interface_types( row_length, rows, land_pts, ntiles,       &
+                                     sm_levels )
+      USE cable_common_module, ONLY : cable_runtime, cable_user
+      
+      INTEGER,INTENT(IN) :: row_length, rows, land_pts, ntiles, sm_levels   
+
+         ALLOCATE( um1%land_index(land_pts) )
+         ALLOCATE( um1%tile_pts(ntiles) )
+         ALLOCATE( um1%tile_frac(land_pts, ntiles) )
+         ALLOCATE( um1%tile_index(land_pts, ntiles) )
+         ALLOCATE( um1%latitude(row_length, rows) )
+         ALLOCATE( um1%longitude(row_length, rows) )
+         ALLOCATE( um1%l_tile_pts(land_pts, ntiles) ) 
+        !-------------------------------------------------------
+         ALLOCATE( kblum_rad%sw_down_dir(row_length,rows) )
+         ALLOCATE( kblum_rad%sw_down_dif(row_length,rows) )
+         ALLOCATE( kblum_rad%sw_down_vis(row_length,rows) )
+         ALLOCATE( kblum_rad%sw_down_nir(row_length,rows) )
+         ALLOCATE( kblum_rad%fbeam(row_length,rows,3) )
+         ALLOCATE( kblum_veg%htveg(land_pts,ntiles) )
+         ALLOCATE( kblum_veg%laift(land_pts,ntiles) )
+         ALLOCATE( kblum_veg%ivegt(land_pts,ntiles) )
+         ALLOCATE( kblum_veg%isoilm(land_pts,ntiles) ) 
+         
+END SUBROUTINE alloc_um_interface_types 
+
+!========================================================================
+!========================================================================
+!========================================================================
+
+END MODULE cable_um_tech_mod
+
+
+
+
