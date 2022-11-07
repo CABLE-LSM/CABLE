1c0a8d7aa48fa758ef3713662cc763a7f961cac5
har917 <har917@users.noreply.github.com>
2022-11-07 04:59:40 +0000

    Deploy preview for PR 51 🛫

--- pr-preview/pr-51/api/src/cbl_lai_eff.F90
+++ pr-preview/pr-51/api/src/cbl_lai_eff.F90
@@ -11,6 +11,20 @@ CONTAINS
 SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
                     reducedLAIdue2snow ) 
 
+  !* Subroutine computes the leaf-area index of a canopy when there is snow
+  ! present.  This subroutine assumes that leaf area is distributed uniformly
+  ! in the vertical.
+  !
+  ! inputs:  <ul> <li> mp - number of land points</li>
+  !           <li> LAI_PFT (mp) - leaf area with no snow (in m2/m2) </li>
+  !           <li> HGT_pft(mp) - height of canopy with no snow (in m)</li>
+  !           <li> HgtAboveSnow (mp) - height of canopy above snow surface (in m)</li></ul>
+  !
+  !  outputs: <ul> <li> reducedLAIdue2snow (m2/m2) - leaf area above snow (in m2/m2) </li></ul>
+  !
+  !           output variable formerly known as rough%hruff
+  !
+
   !re-decl input args  
   integer  :: mp
   real :: LAI_PFT(mp)
@@ -26,7 +40,19 @@ SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
   FracOfCanopyAboveSnow = HgtAboveSnow/ MAX( 0.01, Hgt_PFT)
   
   ! LAI decreases due to snow:
-  reducedLAIdue2snow = LAI_PFT * FracOfCanopyAboveSnow 
+  reducedLAIdue2snow = LAI_PFT * FracOfCanopyAboveSnow
+  !* leaf area acconting for the presence of snow is given by
+  !
+  ! \( LAI_{snow} = LAI_{nosnow} * h_{c,snow} / \max[0.01, h_{c,nosnow} \)
+  !
+  ! where \(h_{c,snow}\) is evaluated in cbl_HgtAboveSnow.F90.
+  !  The LAI is decreased proportionally to the canopy height with/without snow.
+  !
+  ! The effective canopy height takes a minimum value of 0.01m.
+  !
+  ! **There is potential for misbehaviour - if  \( h_{c,snow} < 0.01 \)
+  ! then LAI could be increased (unphysical).  \(h_{c,snow} \ge 10 z{0,min}\)
+  ! only which need not be greater than 0.01m.
 
 END SUBROUTINE LAI_eff
 
