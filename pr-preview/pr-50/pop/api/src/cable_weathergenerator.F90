065321adba1c1140431084114789e2c60e86928c
yingpingwang <yingpingwang@users.noreply.github.com>
2022-11-07 23:12:50 +0000

    Deploy preview for PR 50 🛫

--- pr-preview/pr-50/pop/api/src/cable_weathergenerator.F90
+++ pr-preview/pr-50/pop/api/src/cable_weathergenerator.F90
@@ -266,10 +266,10 @@ WG%WindLite  = WG%WindDay / &
 ! TempMaxDay                                                        Tx
 ! TempMinDayNext                                                    Tp
 ! TempMaxDayPrev  
-!!$write(71, "( 1000e16.6)") WG%DecRad
-!!$   write(72, "( 1000e16.6)") TAN(WG%DecRad)
-!!$   write(73, "( 1000e16.6)") TAN(WG%LatRad)
-!!$ write(74, "( 1000e16.6)")  WG%DayLength 
+!$write(71, "( 1000e16.6)") WG%DecRad
+!$   write(72, "( 1000e16.6)") TAN(WG%DecRad)
+!$   write(73, "( 1000e16.6)") TAN(WG%LatRad)
+!$ write(74, "( 1000e16.6)")  WG%DayLength 
 WHERE ( WG%DayLength .LT. 0.01_sp )
    ! Polar night
    WG%TimeSunrise     = 9._sp              ! Hn
