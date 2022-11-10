871cc7ddd0d839524fc5224b2149041c553db67d
Claire Carouge <claire.carouge@anu.edu.au>
2022-11-11 09:19:11 +1100

    Alternate way to present the documentation

--- src/science/roughness/cbl_hgtAbove_snow.F90
+++ src/science/roughness/cbl_hgtAbove_snow.F90
@@ -9,69 +9,56 @@ CONTAINS
 subroutine HgtAboveSnow( HeightAboveSnow, mp, z0surf_min, HGT_pft, &
      SnowDepth, SnowDensity )
 
-  !* Subroutine computes height of canopy above ground/snow surface when there
+  !* Computes the height of the canopy above ground/snow surface when there
   !  is snow present
   !
-  !  inputs:
+  ! The height of canopy above snow level is simply the difference between
+  ! the height of the canopy above the ground and the depth of snow.
+  ! \[ HgtAboveSnow\_comp = HGT\_pft - SnowHeight \]
+  ! The snow height is obtained from the snow amount and the snow density:
+  ! \[ SnowHeight = 1.2 * \frac{SnowDepth}{SnowDensity} \]
+  ! **the multipler 1.2 needs to be followed up**
   !
-  !  * mp - number of land points
-  !  * z0surf_min - minimum roughness length of surface (in m)
-  !  * HGT_pft(mp) - height of canopy with no snow (in m)
-  !  * SnowDepth(mp) - depth of snow (in mm/m2 liquid water)
-  !  * SnowDensity(mp) - density of snow (in kg/m3)
-  !
-  !  outputs:
-  !
-  !  * HeightAboveSnow (m) - effective height of canopy (in m)
-  !
-  !  The output variable was formerly known as rough%hruff
-  !
+  ! Additionally, we ensure the snow density is larger than a minimum and the
+  ! canopy height above snow is higher than 10 times the minimum surface
+  ! roughness.
 
   implicit none
 
   !re-decl input args  
-  integer  :: mp
+  integer  :: mp !! number of land points (-)
   !result to return
-  real :: HeightAboveSnow(mp) 
+  real :: HeightAboveSnow(mp)  
+    !! Effective height of canopy (output) (m) (also stored in rough%hruff)
   
   !re-decl input args  
-  real :: z0surf_min
-  real :: HGT_pft(mp) 
-  real :: SnowDepth(mp) !snow depth (liquid water ) 
-  real :: SnowDensity(mp) 
+  real :: z0surf_min !! minimum roughness length of surface (m)
+  real :: HGT_pft(mp)  !! height of canopy with no snow (m)
+  real :: SnowDepth(mp) !! amount of snow (\(mm/m^2\) liquid water)
+  real :: SnowDensity(mp) !! density of snow (\(kg/m^3\))
 
   !local_vars: 
-  real, parameter  :: fmin = 10.             
-  real, parameter  :: SnowDensity_min = 100. 
-  !* two internal parameters: fmin = 10 (scalar multiplier)
-  !      and SnowDensity_min = 100.0 (in kg/m3)
+  real, parameter  :: fmin = 10.    ! scalar multiplier         
+  real, parameter  :: SnowDensity_min = 100. ! min. snow density (kg/m3)
   
   real :: SnowDensity_eff(mp) 
   real :: HgtAboveSnow_min(mp)  
   real :: HgtAboveSnow_comp(mp)  
 
+  !|### Structure
+  ! * restricts the Effective snow density to be >= "a" set minimum
   SnowDensity_eff= max( SnowDensity_min, SnowDensity )
-  ! restricts the Effective snow density to be .GE. "a" minimum
   
+  !! * evaluates the mininimum allowed canopy height 
+  !!  (fixed at 10 * min. surface roughness)
   HgtAboveSnow_min =  fmin * z0surf_min
-  ! evaluates min. allowed canopy height (fixed @ 10* min. surface roughness)
-   
-  HgtAboveSnow_comp =   HGT_pft - ( 1.2 * SnowDepth / SnowDensity_eff )
-  !* The height of canopy above snow level is evaluated from snow amount,
-  !  density of snow and input canopy height, while applying a minimum value
-  !
-  ! \( h_{c,abovesnow} = \max[10 z_{0,min}, h_{c,nosnow} - 1.2 d_{snow}/\rho_{snow}] \)
-  !
-  !  A minimum value for the effective canopy height of
-  !  '10* the minimum roughness length of the surface' is applied
-  !  (a numerics requirement).
-  !  Converting snow amount to snow depth requires the snow density;
-  !  the snow density has a minimum value of 100 kg/m3 enforced.
-  !
-  !  **the multipler 1.2 needs to be followed up**
 
+  !! * calculates the height of the canopy above snow 
+  HgtAboveSnow_comp =   HGT_pft - ( 1.2 * SnowDepth / SnowDensity_eff )
+
+  !* * returns the effective canopy height above snow after comparison to
+  ! the minimum allowed canopy height.
   HeightAboveSnow = max( HgtAboveSnow_min,  HgtAboveSnow_comp )
-  ! Finally return the Effective canopy height above snow 
 
   return
 
