6ccc4f530e0179ebd9bd84d06ce690029e330551
ccarouge <ccarouge@users.noreply.github.com>
2022-11-15 01:03:17 +0000

    Deploy preview for PR 74 🛫

--- pr-preview/pr-74/api/src/cbl_lai_eff.F90
+++ pr-preview/pr-74/api/src/cbl_lai_eff.F90
@@ -16,21 +16,6 @@ SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
   !* This subroutine computes the leaf-area index of a canopy when there is snow
   ! present. The formulae assume that the leaf area is distributed uniformly
   ! in the vertical within the canopy.
-  !
-  ! inputs:
-  !
-  ! * `mp` - number of land points
-  ! * `LAI_PFT (mp)` - leaf area with no snow (m\(^2\) m\(^{-2})\)
-  ! * `HGT_pft (mp)` - height of canopy with no snow (m)
-  ! * `HgtAboveSnow (mp)` - height of canopy above snow surface (m) 
-  !
-  ! outputs:
-  !
-  ! * `reducedLAIdue2snow (mp)` - modified leaf area for snow
-  !   (m\(^2\) m\(^{-2}\)))
-  !
-  ! `reducedLAIdue2snow` is known as `rough%vlaiw` elsewhere in the code.
-  ! <br></br>
 
   !re-decl input args  
   integer  :: mp !! Number of land points (-)
@@ -38,7 +23,7 @@ SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
   real :: Hgt_PFT(mp) !! height of canopy without snow (m)
   real :: HgtAboveSnow(mp) !! height of canopy above the snow surface (m)
   real :: reducedLAIdue2snow(mp) 
-   !! Output. Modified leaf area index for snow (m\(^2\) m\(^{-2}\)))
+   !! Output. Modified leaf area index for snow (m\(^2\) m\(^{-2}\))
  
   !local_vars: 
   real :: Hgt_eff(mp)
@@ -51,7 +36,7 @@ SUBROUTINE LAI_eff( mp, LAI_PFT, Hgt_PFT, HgtAboveSnow,  &
   reducedLAIdue2snow = LAI_PFT * FracOfCanopyAboveSnow
   !* Leaf area, LAI, accounting for the presence of snow, is given by
   !
-  ! \[ LAI_{snow} = LAI_{nosnow} h_{c,snow} / \max[0.01, h_{c,nosnow} ] \]
+  ! \[ LAI_{snow} = LAI_{nosnow} \frac{h_{c,snow}}{\max(0.01, h_{c,nosnow})} \]
   !
   ! where \(h_{c,snow}\) is evaluated in subroutine [[HgtAboveSnow]].
   ! The LAI is decreased proportionally to the canopy height with/without snow.
