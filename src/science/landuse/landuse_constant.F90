6c84f2600bf6b80b1569008c3cb3fcdaaba9f49f
Tammas Loughran <tammas.loughran@csiro.au>
2022-12-13 12:20:38 +1100

    Added units documentation to landuse_constant module and WARNING note.

--- src/science/landuse/landuse_constant.F90
+++ src/science/landuse/landuse_constant.F90
@@ -1,29 +1,31 @@
 MODULE landuse_constant
-  ! this contains landuse-specific constants
+  !! The `landuse_constant` module contains landuse-specific constants.
+  !!
+  !! **WARNING:** Eventually, `mvmax` in the land-use code should be replaced with `mvtype`
   ! written by YP Wang
   USE cable_def_types_mod,    ONLY: mvtype,mstype,mland,ms,msn,nrb,ncp,ncs,r_2
   USE casadimension,          ONLY: icycle,mplant,mlitter,msoil,mwood,mso
   IMPLICIT NONE
   !  integer, parameter                     :: sp =selected_real_kind(8)
   !  integer, parameter                     :: dp =selected_real_kind(16)
-  !| number of land use states as in HYDE land use dataset processed by  
+  INTEGER,   PARAMETER                   :: mstate   = 12
+  !* Number of land use states as in HYDE land use dataset processed by 
   ! [George Hurtt](https://doi.org/10.5194/gmd-13-5425-2020)
-  !
-  INTEGER,   PARAMETER                   :: mstate   = 12  ! number of land use states
-  !> max number of PFT in CABLE, should be replaced by "mvtype" 
+  ! \( (-) \)
   INTEGER,   PARAMETER                   :: mvmax    = 17
-  !> number of harvest types
+  !! Maximum number of plant functional types in CABLE  \( (-) \)
   INTEGER,   PARAMETER                   :: mharvw   = 5           
-  !> threshold area fraction for a separate tile within a land cell
+  !! Number of harvest types \( (-) \)
   real(r_2),    PARAMETER                   :: thresh_frac  = 1.0e-6
-  !> allocation of harvested wood into different wood product pools
+  !! Threshold area fraction for a separate tile within a land cell \( (-) \)
   REAL(r_2),    PARAMETER, DIMENSION(mwood) :: fwoodprod    =(/0.3,0.4,0.4/)
-  !> decay rate of different wood product pools
+  !! Allocation of harvested wood into each wood product pools \( (-) \)
   REAL(r_2),    PARAMETER, DIMENSION(mwood) :: ratewoodprod =(/1.0,0.1,0.01/)
-  !> biomass fraction in leaf wood and root for woody seedlings in a cleared area
+  !! Decay rate of each wood product pools \( ( year^{-1} ) \)
   REAL(r_2),    PARAMETER, DIMENSION(mwood) :: fracwoodseed =(/0.4,0.3,0.3/)
-  !> biomass fraction in leaf wood and root for grassy seedlings in a cleared area
+  !! Biomass fraction in leaf, wood and root pools for woody seedlings in a cleared area \( (-) \)
   REAL(r_2),    PARAMETER, DIMENSION(mwood) :: fracgrassseed=(/0.6,0.0,0.4/)
-  !> total biomass of seeldings in g C/m2 in a cleared area
+  !! Biomass fraction in leaf wood, and root pools for grassy seedlings in a cleared area \( (-) \)
   REAL(r_2),    PARAMETER                   :: fseedling = 0.001
+  !! Total biomass of seeldings in a cleared area \( ( g C \cdot m^{-2} ) \)
 END MODULE landuse_constant
