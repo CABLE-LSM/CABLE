c94956036bdcf0db703e92cbdcb3f8fdf69e3599
har917 <94580228+har917@users.noreply.github.com>
2022-12-13 14:00:48 +1100

    Update src/science/canopy/cbl_LAI_canopy_height.F90
    Co-authored-by: Claire Carouge <ccarouge@users.noreply.github.com>

--- src/science/canopy/cbl_LAI_canopy_height.F90
+++ src/science/canopy/cbl_LAI_canopy_height.F90
@@ -27,7 +27,7 @@ subroutine limit_HGT_LAI( HGT_pft_temp, LAI_pft_cbl, HGT_pft_cbl, mp, land_pts, 
   ! canopy height `HGT_pft` are checked for
   !
   ! - whether the land_pt has a non-zero fraction of that tile (if zero
-  !   fraction then LAI and canopy height are set to zero.
+  !   fraction then LAI and canopy height are set to zero)
   ! - whether the LAI lies above a minimum value `CLAI_thresh`
   ! - whether the canopy height lies a minimum value (which are PFT dependent)
   ! - whether non-vegetated tiles have a non-zero canopy height.
