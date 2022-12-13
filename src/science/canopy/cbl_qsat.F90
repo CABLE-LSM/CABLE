e49acce03d86e69ed53d0abfb3d68f1226036eb7
Ian Harman <inh599@gadi-login-08.gadi.nci.org.au>
2022-12-13 16:31:11 +1100

    first comments on cbl_qsat

--- src/science/canopy/cbl_qsat.F90
+++ src/science/canopy/cbl_qsat.F90
@@ -1,19 +1,35 @@
 MODULE cbl_qsat_module
+  !* This MODULE contains two SUBROUTINEs that calculate
+  ! the specific humdity at saturation as a function of air pressure
+  ! and temperature.
+  !
+  ! The two SUBROUTINEs differ only in that [[qsatfjh]] operates on an array
+  ! whereas as [[qsatjfh2]] operates on single element REALs.
 
 PUBLIC qsatfjh
 PUBLIC qsatfjh2
 
 CONTAINS
 
-SUBROUTINE qsatfjh(mp, var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+  SUBROUTINE qsatfjh(mp, var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
+    !*This SUBROUTINE evaluates the specific humdity (water vapour mixing ratio)
+    ! at saturation (in kgkg\(^{-1}\), at air temperature (in \(^{\circ}\)C)
+    ! and air pressure (in hPa) across an array of size mp.
+    !
+    !**METHOD
+    !
+    ! The standard formula for specific humdity at saturation is used,
+    ! see e.g. Garratt (1995), based on the values for the mass of a mole
+    ! of water,`CRMH2o`, mass of a mole of dry air `Crmair`,
+    ! and the Teten constants.
 
 IMPLICIT  NONE   
-integer :: mp 
-REAL, INTENT(OUT) :: var(mp)         ! result; sat sp humidity
+integer :: mp                        !! size of array of land points
+REAL, INTENT(OUT) :: var(mp)         !! sat specific humidity (kgkg\(^{-1}\))
 REAL :: CRMH2o, Crmair, CTETENA, CTETENB, CTETENC
 REAL, INTENT(IN) ::                                          &
-  tair(mp),                        & ! air temperature (C)
-  pmb(mp)                            ! pressure PMB (mb)
+  tair(mp),                        & !! air temperature (\(^{\circ}\)C)
+  pmb(mp)                            !! surface air pressure (hPa)
 
 INTEGER :: j
 
@@ -28,14 +44,24 @@ END SUBROUTINE qsatfjh
 
 
 SUBROUTINE qsatfjh2( var, CRMH2o, Crmair, CTETENA, CTETENB, CTETENC, tair,pmb)
-
+  !*This SUBROUTINE evaluates the specific humdity (water vapour mixing ratio)
+  ! at saturation (in kgkg\(^{-1}\), at air temperature (in \(^{\circ}\)C and
+  ! and air pressure (in hPa).
+  !
+  !** METHOD
+  !
+  ! The standard formula for specific humdity at saturation is used,
+  ! see e.g. Garratt (1995), based on the values for the mass of a mole
+  ! of water,`CRMH2o`, mass of a mole of dry air `Crmair`,
+  ! and the Teten constants.
+  
 REAL :: CRMH2o, Crmair, CTETENA, CTETENB, CTETENC
   REAL, INTENT(IN) ::                                                         &
-       tair,         & ! air temperature (C)
-       pmb             ! pressure PMB (mb)
+       tair,         & !! air temperature (\(^{\circ}\)C),
+       pmb             !! surface air pressure (hPa)
 
   REAL, INTENT(OUT) ::                                                        &
-       var             ! result; sat sp humidity
+       var             !! specific humidity at saturation (kgkg\(^{-1}\))
 
   var = (CRMH2o/Crmair) * (CTETENA*EXP(CTETENB*tair/(CTETENC+tair))) / pmb
 
