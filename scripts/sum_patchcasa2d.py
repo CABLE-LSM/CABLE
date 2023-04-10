15c285a874cbde74997241b2ecdfd0772ac54221
moc801 <moc801@b483b3d9-cc7d-4f55-9e4c-b4562452379e>
2023-04-10 22:12:28 +0000

    Added x and y variables to 2d output of casa
    git-svn-id: https://trac.nci.org.au/svn/cable/branches/Share/CABLE-POP_TRENDY@9542 b483b3d9-cc7d-4f55-9e4c-b4562452379e

--- scripts/sum_patchcasa2d.py
+++ scripts/sum_patchcasa2d.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import print_function
 """
 usage: sum_patchcasa2d.py [-h] [-o output_netcdf] [-v] [-z] [input_netcdf]
 
@@ -27,10 +26,13 @@ Example
 History
 -------
 Written  Matthias Cuntz, May 2020
-    - from sum_patchcasa.py and nopatch2d.py
+             - from sum_patchcasa.py and nopatch2d.py
 Modified Matthias Cuntz, Jul 2022
-    - flake8 compatible
-    - Take into account patchfrac, which is Casa output since rev 9053
+             - flake8 compatible
+             - Take into account patchfrac, which is Casa output since rev 9053
+         Matthias Cuntz, Apr 2023
+             - add x and y variables
+
 """
 import sys
 import argparse
@@ -106,21 +108,23 @@ else:
 # patchfrac with different dimensions
 # time is always the first and land the last dimension
 patchfrac = fi.variables['patchfrac'][:]
-patchfrac1 = patchfrac[0, :]
-patchfrac2 = patchfrac[:, :]
-patchfrac3 = patchfrac[:, np.newaxis, :]
-patchfrac4 = patchfrac[:, np.newaxis, np.newaxis, :]
+patchfrac1d = patchfrac[0, :]
+patchfrac2d = patchfrac[:, :]
+patchfrac3d = patchfrac[:, np.newaxis, :]
+patchfrac4d = patchfrac[:, np.newaxis, np.newaxis, :]
 
 # Determine number of land points
-lats = fi.variables['latitude'][:]
+lats = fi.variables['latitude'][:]  # dim land
 lons = fi.variables['longitude'][:]
-sidx = np.zeros(lats.size, dtype=int)
-lidx = np.ones(lats.size, dtype=int)
+sidx = np.zeros(lats.size, dtype=int)  # index of new grid cells
+lidx = np.ones(lats.size, dtype=int)   # no. of patches per grid cell
 nidx = 0
 for i in range(1, lons.size):
     if (lons[i] == lons[i - 1]) and (lats[i] == lats[i - 1]):
+        # next patch of same grid cell
         lidx[nidx] += 1
     else:
+        # new grid cell
         nidx += 1
         sidx[nidx] = i
         lidx[nidx] = 1
@@ -129,7 +133,7 @@ lidx = lidx[:nidx]
 
 # Get latitude/longitude indices
 # input grid
-ilats = fi.variables['latitude'][sidx]
+ilats = fi.variables['latitude'][sidx]  # dim no. of grid cells
 ilons = fi.variables['longitude'][sidx]
 nland = ilats.size
 # output grid
@@ -145,33 +149,60 @@ olon = (-180. + clon + np.arange(nlon) / float(nlon - 1) *
         (360. - dlon))  # new lons
 olon2d, olat2d = np.meshgrid(olon, olat)  # new lats, lons in 2D
 lltree = cp.llKDTree(olat2d, olon2d)  # KD-tree
-iidl   = np.arange(nland, dtype=int)  # indices of land in input grid
-oidx   = np.empty(nland, dtype=int)  # indices of lon in output grid
-oidy   = np.empty(nland, dtype=int)  # indices of lat in output grid
+iidl = np.arange(nland, dtype=int)  # indices of land in input grid
+oidx = np.empty(nland, dtype=int)   # indices of lon in output grid
+oidy = np.empty(nland, dtype=int)   # indices of lat in output grid
 for i in range(nland):
     iy, ix = lltree.query(ilats[i], ilons[i])
     oidx[i] = ix
     oidy[i] = iy
 
 # Copy global attributes, adding script
-cp.set_global_attributes(fi, fo,
-                         add={'history': (ptime.asctime() + ': ' +
-                                          ' '.join(sys.argv))})
+cp.set_global_attributes(
+    fi, fo, add={'history': (ptime.asctime() + ': ' + ' '.join(sys.argv))})
 
 # Copy dimensions
 cp.create_dimensions(fi, fo, removedim=['land', 'ntile'],
                      adddim={'x': nlon, 'y': nlat})
 
 # create static variables (independent of time)
-cp.create_variables(fi, fo, time=False, izip=izip, fill=True, chunksizes=False,
+cp.create_variables(fi, fo, time=False, izip=izip, fill=True,
+                    chunksizes=False,
                     replacedim={'land': ('y', 'x'), 'ntile': ('y', 'x')})
+
+# create x and y variables
+if 'x' not in fi.variables:
+    xattr = {'standard_name': 'longitude',
+             'long_name': 'longitude',
+             'units': 'degrees_east',
+             'axis': 'X'}
+    x = fo.createVariable('x', 'f4', ('x',))
+    for a in xattr:
+        x.setncattr(a, xattr[a])
+
+if 'y' not in fi.variables:
+    yattr = {'standard_name': 'latitude',
+             'long_name': 'latitude',
+             'units': 'degrees_north',
+             'axis': 'Y'}
+    y = fo.createVariable('y', 'f4', ('y',))
+    for a in yattr:
+        y.setncattr(a, yattr[a])
+
 # create dynamic variables (time dependent)
-cp.create_variables(fi, fo, time=True, izip=izip, fill=True, chunksizes=False,
+cp.create_variables(fi, fo, time=True, izip=izip, fill=True,
+                    chunksizes=False,
                     replacedim={'land': ('y', 'x'), 'ntile': ('y', 'x')})
 
 #
 # Copy variables from in to out, summing the patches and make lat/lon
 #
+
+# set x and y
+if 'x' not in fi.variables:
+    fo.variables['x'][:] = olon
+if 'y' not in fi.variables:
+    fo.variables['y'][:] = olat
 
 # copy static and dynamic variables
 if verbose:
@@ -192,13 +223,13 @@ for ivar in fi.variables.values():
     if ('land' in ivar.dimensions) or ('ntile' in ivar.dimensions):
         # sum patch
         if len(invar.shape) == 1:
-            ipatchfrac = patchfrac1
+            ipatchfrac = patchfrac1d
         elif len(invar.shape) == 2:
-            ipatchfrac = patchfrac2
+            ipatchfrac = patchfrac2d
         elif len(invar.shape) == 3:
-            ipatchfrac = patchfrac3
+            ipatchfrac = patchfrac3d
         elif len(invar.shape) == 4:
-            ipatchfrac = patchfrac4
+            ipatchfrac = patchfrac4d
         outshape     = list(invar.shape)
         outshape[-1] = nlat   # patch to y,x
         outshape.append(nlon)
