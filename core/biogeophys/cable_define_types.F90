ebeb1c7b06a413d67b614173aa91d03149756883
Zihan Lu <zihan.lu@inrae.fr>
2024-05-23 16:56:22 +0200

    just delete the testzlu variable

--- core/biogeophys/cable_define_types.F90
+++ core/biogeophys/cable_define_types.F90
@@ -84,8 +84,6 @@ module cable_def_types_mod
   ! general functions
   public :: nc_err
 
-  private :: testzlu
-
   ! .............................................................................
 
   ! Energy and water balance variables:
