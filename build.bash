7326db351e0f496dd484639c69d6dec59a0bfae3
Sean Bryan <39685865+SeanBryan51@users.noreply.github.com>
2024-06-17 18:32:59 +1000

    Port branch CABLE-POP_TRENDY to CMake (#238)
    PR for porting the CABLE-POP_TRENDY branch to CMake.
    
    **Context:**
    
    - We want to consolidate the build systems across CABLE to use CMake to
    improve ease of testing with tools such as [benchcab][benchcab] and
    [spack][spack].
    - With the current CABLE-4 development around CABLE-POP_TRENDY, it makes
    sense to port this branch first.
    
    **Checklist:**
    
    - [x] Review compiler flags for default, release and debug
    configurations for the following compilers in `CABLE-POP_TRENDY` and
    compare against build configurations used in `main` (see #268):
        1. Intel compiler (classic)
        2. Intel compiler (llvm)
        3. GNU compiler
    - [x] Ensure model outputs are bitwise identical between Makefile and
    CMake builds of CABLE-POP_TRENDY on Gadi.
    - Model outputs were generated using the [TRENDY_V12][TRENDY_V12]
    configuration for a reduced domain (`extent="64.0,66.0,60.0,62.0"` in
    `run_TRENDY.sh`). Model outputs were compared using `nccmp -d <file_a>
    <file_b>`. The model outputs being compared are all
    `cru_out_cable_1901_2022.nc` and `cru_out_casa_1901_2022.nc` outputs (as
    these two files are the final products) for each serial CABLE run.
    - [x] Ensure compiled object files are identical between Makefile and
    CMake builds of CABLE-POP_TRENDY on Gadi.
    
    [benchcab]: https://github.com/CABLE-LSM/benchcab
    [spack]: https://spack.io/
    [TRENDY_V12]: https://github.com/CABLE-LSM/TRENDY_V12

---
+++ build.bash
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+
+ncpus_default=4
+
+script_name=$(basename "${0}")
+
+show_help() {
+    cat << EOF
+Usage: ./${script_name} [OPTIONS]
+
+Build script wrapper around CMake. Supplied arguments that do not match the
+options below will be passed to CMake when generating the build system.
+
+Options:
+  -c, --clean   Delete build directory before invoking CMake.
+  -m, --mpi     Compile MPI executable.
+  -C, --compiler <compiler>
+                Specify the compiler to use.
+  -n, --ncpus <ncpus>
+                Specify the number of parallel jobs in the compilation. By
+                default this value is set to ${ncpus_default}.
+  -h, --help    Show this screen.
+
+Enabling debug mode:
+
+  The release build is default. To enable debug mode, specify the CMake option
+  -DCMAKE_BUILD_TYPE=Debug when invoking ${script_name}.
+
+Enabling verbose output from Makefile builds:
+
+  To enable more verbose output from Makefile builds, specify the CMake option
+  -DCMAKE_VERBOSE_MAKEFILE=ON when invoking ${script_name}.
+
+EOF
+}
+
+cmake_args=(-DCMAKE_BUILD_TYPE=Release -DCABLE_MPI=OFF)
+
+# Argument parsing adapted and stolen from http://mywiki.wooledge.org/BashFAQ/035#Complex_nonstandard_add-on_utilities
+while [ ${#} -gt 0 ]; do
+    case ${1} in
+        -c|--clean)
+            rm -r bin build
+            exit
+            ;;
+        -m|--mpi)
+            mpi=1
+            cmake_args+=(-DCABLE_MPI="ON")
+            ;;
+        -C|--compiler)
+            compiler=${2}
+            shift
+            ;;
+        -n|--ncpus)
+            CMAKE_BUILD_PARALLEL_LEVEL=${2}
+            shift
+            ;;
+        -h|--help)
+            show_help
+            exit
+            ;;
+        ?*)
+            cmake_args+=("${1}")
+            ;;
+    esac
+    shift
+done
+
+if hostname -f | grep gadi.nci.org.au > /dev/null; then
+    : "${compiler:=intel}"
+
+    . /etc/bashrc
+    module purge
+    module add cmake/3.24.2
+    module add netcdf/4.9.2
+    case ${compiler} in
+        intel)
+            module add intel-compiler-llvm/2023.0.0
+            compiler_lib_install_dir=Intel
+            [[ -n ${mpi} ]] && module add intel-mpi/2019.5.281
+
+            # This is required to use ifort instead of ifx (default):
+            cmake_args+=(-DCMAKE_Fortran_COMPILER="ifort")
+
+            # This is required to build with runtime dispatch for all
+            # architectures on Gadi (see
+            # https://opus.nci.org.au/display/Help/Sapphire+Rapids+Compute+Nodes
+            # for more information):
+            cmake_args+=(-DCMAKE_Fortran_FLAGS_RELEASE_INIT="-march=broadwell -axSKYLAKE-AVX512,CASCADELAKE,SAPPHIRERAPIDS -diag-disable=15009")
+            ;;
+        gnu)
+            module add gcc/13.2.0
+            compiler_lib_install_dir=GNU
+            [[ -n ${mpi} ]] && module add openmpi/4.1.4
+            ;;
+        ?*)
+            echo -e "\nError: compiler ${compiler} is not supported.\n"
+            exit 1
+            ;;
+    esac
+
+    # This is required so that the netcdf-fortran library is discoverable by
+    # pkg-config:
+    prepend_path PKG_CONFIG_PATH "${NETCDF_BASE}/lib/${compiler_lib_install_dir}/pkgconfig"
+
+    if module is-loaded openmpi; then
+        # This is required so that the openmpi MPI libraries are discoverable
+        # via CMake's `find_package` mechanism:
+        prepend_path CMAKE_PREFIX_PATH "${OPENMPI_BASE}/include/${compiler_lib_install_dir}"
+    fi
+
+elif hostname -f | grep -E '(mc16|mcmini)' > /dev/null; then
+    : "${compiler:=gnu}"
+
+    case ${compiler} in
+        gnu)
+            export PKG_CONFIG_PATH=/usr/local/netcdf-fortran-4.6.1-gfortran/lib/pkgconfig:${PKG_CONFIG_PATH}
+            export PKG_CONFIG_PATH=${HOMEBREW_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
+            cmake_args+=(-DCMAKE_Fortran_COMPILER=gfortran)
+            ;;
+        ?*)
+            echo -e "\nError: compiler ${compiler} is not supported.\n"
+            exit 1
+            ;;
+    esac
+fi
+
+export CMAKE_BUILD_PARALLEL_LEVEL="${CMAKE_BUILD_PARALLEL_LEVEL:=${ncpus_default}}"
+
+cmake -S . -B build "${cmake_args[@]}" &&\
+cmake --build build &&\
+cmake --install build --prefix .
