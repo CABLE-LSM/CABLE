826236816cb29c7fba7b7fa8ca6fd8fa70cd4405
Sean Bryan <sean.bryan@anu.edu.au>
2024-10-24 16:49:15 +1100

    Move MPI initialisation into cable_driver_init

--- src/offline/cable_driver_init.F90
+++ src/offline/cable_driver_init.F90
@@ -1,6 +1,10 @@
 MODULE cable_driver_init_mod
   !! Module for CABLE offline driver initialisation.
   USE cable_namelist_util, ONLY : get_namelist_file_name
+#ifdef __MPI__
+  USE mpi
+  USE cable_mpicommon, ONLY : comm, rank
+#endif
   IMPLICIT NONE
   PRIVATE
 
@@ -10,6 +14,9 @@ CONTAINS
 
   SUBROUTINE cable_driver_init()
     !! Model initialisation routine for the CABLE offline driver.
+#ifdef __MPI__
+    INTEGER :: np, ierr
+#endif
 
     !check to see if first argument passed to cable is
     !the name of the namelist file
@@ -17,9 +24,16 @@ CONTAINS
     CALL get_namelist_file_name()
 
 #ifdef __MPI__
-    ! MPI specific initialisation
-#else
-    ! Serial specific initialisation
+    CALL MPI_Init(ierr)
+    CALL MPI_Comm_dup(MPI_COMM_WORLD, comm, ierr)
+    CALL MPI_Comm_size(comm, np, ierr)
+
+    IF (np < 2) THEN
+      WRITE (*,*) 'This program needs at least 2 processes to run!'
+      CALL MPI_Abort(comm, 0, ierr)
+    END IF
+
+    CALL MPI_Comm_rank(comm, rank, ierr)
 #endif
 
   END SUBROUTINE cable_driver_init
--- src/offline/cable_mpidrv.F90
+++ src/offline/cable_mpidrv.F90
@@ -22,27 +22,16 @@ PROGRAM mpi_driver
   USE mpi
   USE cable_driver_init_mod
 
-  USE cable_mpicommon
+  USE cable_mpicommon, ONLY : comm, rank
   USE cable_mpimaster
   USE cable_mpiworker
 
   IMPLICIT NONE
 
-  INTEGER :: comm, np, rank, ierr
+  INTEGER :: ierr
   REAL    :: etime ! Declare the type of etime()
 
   CALL cable_driver_init()
-
-  CALL MPI_Init (ierr)
-  CALL MPI_Comm_dup (MPI_COMM_WORLD, comm, ierr)
-  CALL MPI_Comm_size (comm, np, ierr)
-
-  IF (np < 2) THEN
-     WRITE (*,*) 'This program needs at least 2 processes to run!'
-     CALL MPI_Abort (comm, 0, ierr)
-  END IF
-
-  CALL MPI_Comm_rank (comm, rank, ierr)
 
   IF (rank == 0) THEN
      CALL mpidrv_master (comm)
