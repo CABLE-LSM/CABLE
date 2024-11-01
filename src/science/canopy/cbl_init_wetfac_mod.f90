487ed5321958f96b57cba32b292dd055014f6139
Ben Schroeter <benjschroeter@gmail.com>
2024-11-01 14:56:08 +1100

    Review comments. Fixes #219

--- src/science/canopy/cbl_init_wetfac_mod.f90
+++ src/science/canopy/cbl_init_wetfac_mod.f90
@@ -1,4 +1,6 @@
 MODULE cable_init_wetfac_mod
+    !! Module containing subroutine to initialise the surface wetness factor
+    !! of the soil/snow (ssnow_wetfac) array
    
     IMPLICIT NONE
 
@@ -16,11 +18,24 @@ MODULE cable_init_wetfac_mod
         met_tk, &
         Ctfrz &
     )
+        !! ## Purpose
+        !!
+        !! Initialize the surface wetness factor of the soil/snow 
+        !! (ssnow_wetfac) array
+        !!
+        !! ## Method
+        !!
+        !! **Warning**: The original subroutine from which this was ported
+        !! lacks any documented methodolody.
+        !!
+        !! ## References
+        !!
+        !! **Warning**: The original subroutine from which this was ported
+        !! lacks any literature reference.
 
-        !! Initialize the surface wetness factor of the soil/snow (ssnow_wetfac) array
+        
 
         ! Imports
-        ! use cable_surface_types_mod, ONLY: lakes => lakes_cable        
         USE grid_constants_mod_cbl, ONLY: lakes_cable
         USE cable_def_types_mod,  ONLY : r_2
         USE cable_other_constants_mod, ONLY : wilt_limitfactor
@@ -29,15 +44,15 @@ MODULE cable_init_wetfac_mod
 
         ! Arguments (See soil params file for units?)
         INTEGER, INTENT(IN) :: mp !! Number of active land points
-        REAL, INTENT(INOUT) :: ssnow_wetfac(mp) !! Soil/snow wetness factor
-        REAL, INTENT(IN) :: soil_swilt(mp) !! Wilting factor, point at which plants in soil start to wilt (unit?)
-        REAL, INTENT(IN) :: soil_sfc(mp) !! Soil sfc array
-        REAL(r_2), INTENT(IN) :: ssnow_wb(mp) !! Soil/snow moisture
-        REAL(r_2), INTENT(IN) :: ssnow_wbice(mp) !! Frozen component of soil/snow moisture
+        REAL, INTENT(INOUT) :: ssnow_wetfac(mp) !! Surface wetness factor at current time step
+        REAL, INTENT(IN) :: soil_swilt(mp) !! Wilting factor, point at which plants in soil start to wilt
+        REAL, INTENT(IN) :: soil_sfc(mp) !! Volumetric H20 @ field capacity
+        REAL(r_2), INTENT(IN) :: ssnow_wb(mp) !! Volumetric soil moisture (solid+liquid)
+        REAL(r_2), INTENT(IN) :: ssnow_wbice(mp) !! Soil ice
         REAL, INTENT(IN) :: ssnow_snowd(mp) !! Soil/snow snow depth (mm of water)
-        REAL, INTENT(IN) :: met_tk(mp) !! Air temperature (kelvin)
-        REAL, INTENT(IN) :: Ctfrz  !! Freezing temperature (CONSTANT)
-        integer, INTENT(IN) :: veg_iveg(mp) !! Surface types (one of the 17, integer)
+        REAL, INTENT(IN) :: met_tk(mp) !! Air temperature (Kelvin)
+        REAL, INTENT(IN) :: Ctfrz  !! Freezing temperature (Kelvin)
+        INTEGER, INTENT(IN) :: veg_iveg(mp) !! Surface types
 
         ! Local variables
         REAL :: wilting_pt(mp) ! Wilting point
@@ -51,7 +66,7 @@ MODULE cable_init_wetfac_mod
         wilting_pt(:) = soil_swilt(:) / wilt_limitfactor 
         wetfac_num(:) = REAL(ssnow_wb(:)) - wilting_pt(:)
         wetfac_den(:) = REAL(soil_sfc(:) - wilting_pt(:))
-        wetfac_den(:) = MAX(0.0830, wetfac_den(:)) !? What is this magical number? It should be a constant and described
+        wetfac_den(:) = MAX(0.0830, wetfac_den(:)) ! WARNING: CABLE#457
 
         ! Set some "meaningful" defaults
         ssnow_wetfac(:) = wetfac_num(:) / wetfac_den(:)
@@ -65,7 +80,7 @@ MODULE cable_init_wetfac_mod
             IF (ssnow_wbice(i) > 0.0) THEN
                 
                 ! ice_ratio = ice moisture / total moisture (** ?)
-                ice_ratio  = (ssnow_wbice(i) / ssnow_wb(i))**2 ! Why is this squared?
+                ice_ratio  = (ssnow_wbice(i) / ssnow_wb(i))**2
 
                 !~ 1-Ice_ratio^2 
                 ice_factor = 1._r_2 - MIN(0.2_r_2, ice_ratio)
@@ -74,7 +89,7 @@ MODULE cable_init_wetfac_mod
 
             END IF
 
-            ! If snow depth is greater than 0.1m then 90% wet
+            ! If snow depth is greater than 0.1m then soil is at 90% saturation
             IF (ssnow_snowd(i) > 0.1) THEN
                 ssnow_wetfac(i) = 0.9
             END IF
@@ -82,12 +97,12 @@ MODULE cable_init_wetfac_mod
             ! If we are on a lake
             IF (veg_iveg(i) == lakes_cable) THEN
 
-                ! When the air temperature is >= +5 deg above freezing it is 100% wet
+                ! When the air temperature is >= +5 deg above freezing it at 100% saturation
                 IF ( met_tk(i) >= Ctfrz + 5. ) THEN
                     ssnow_wetfac(i) = 1.0
                 END IF
 
-                ! When the air temperature is < +5 deg above freezing it is 70% wet
+                ! When the air temperature is < +5 deg above freezing it is at 70% saturation
                 IF( met_tk(i) < Ctfrz + 5. ) THEN
                     ssnow_wetfac(i) = 0.7
                 END IF
