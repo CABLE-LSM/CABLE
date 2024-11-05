c3b75597ab4fd1e4d7b6b79f957b7eb6257d7cd2
Ramzi Kutteh <98803952+rkutteh@users.noreply.github.com>
2024-11-05 13:06:04 +1100

    Update src/offline/cable_iovars.F90
    Co-authored-by: Claire Carouge <ccarouge@users.noreply.github.com>

--- src/offline/cable_iovars.F90
+++ src/offline/cable_iovars.F90
@@ -139,14 +139,8 @@ MODULE cable_IO_vars_module
        logn          ! log file unit number
 
   LOGICAL ::                                                                  &
-!       verbose,    & ! print init and param details of all grid cells?
-!       soilparmnew   ! read IGBP new soil map. Q.Zhang @ 12/20/2010
-
-! above replaced by below - rk4417 - phase2
-
-       verbose! print init and param details of all grid cells? ! MMY,    & 
-!   soilparmnew   ! read IGBP new soil map. Q.Zhang @ 12/20/2010
-! MMY @Oct2022 change to use soilparmnew by default
+       verbose,    & ! print init and param details of all grid cells?
+       soilparmnew   ! read IGBP new soil map. Q.Zhang @ 12/20/2010
 
 
   ! ================ Veg and soil type variables ============================
