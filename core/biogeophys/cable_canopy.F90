fed12c88b6e2f16f9fc992fb3ee6e165d0d4474e
Zihan Lu <zihan.lu@inrae.fr>
2024-11-10 16:25:22 +0100

    24th try to solve the Segmentation fault - invalid memory reference error in Tuzet & LWP schemes

--- core/biogeophys/cable_canopy.F90
+++ core/biogeophys/cable_canopy.F90
@@ -2185,6 +2185,7 @@ CONTAINS
                      print *, '!!!!!!!!!!!!!!! g2 and psi_ref:', veg%g2(i), veg%psi_ref(i)
                      print *, '!!!!!!!!!!!!!!! psil:', psil(i)
                      fwpsi(i) = (1+exp(veg%g2(i) * veg%psi_ref(i))) / (1+exp(veg%g2(i) * (veg%psi_ref(i)-psil(i))))
+                     print *, '!!!!!!!!!!!!!!! fwpsi:', fwpsi(i)
                      gs_coeff(i,1) =fwpsi(i) * g1 / real(csx(i,1))
                      gs_coeff(i,2) =fwpsi(i) * g1 / real(csx(i,2))
                      print *, 'fwpsi:', fwpsi(i)
