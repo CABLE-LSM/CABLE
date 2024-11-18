28e972e6a5e5dde3697bef7faaec26c2d957aad0
Claire Carouge <claire.carouge@anu.edu.au>
2024-11-18 12:12:38 +1100

    Call GWspatialParameters

--- src/offline/cable_parameters.F90
+++ src/offline/cable_parameters.F90
@@ -67,7 +67,7 @@ MODULE cable_param_module
   PRIVATE
   PUBLIC get_default_params, write_default_params, derived_parameters,         &
        check_parameter_values, report_parameters, parID_type,                &
-       write_cnp_params, consistency_ice_veg_soil 
+       write_cnp_params, consistency_ice_veg_soil, GWspatialParameters 
   INTEGER :: patches_in_parfile=4 ! # patches in default global parameter
   ! file
 
