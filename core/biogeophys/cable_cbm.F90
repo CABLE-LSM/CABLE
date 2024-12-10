2f77c53ae8b462f8925670a155618a665d468433
Zihan Lu <zihan.lu@inrae.fr>
2024-12-10 15:09:32 +0100

    5th test fwpsi in cable_canopy

--- core/biogeophys/cable_cbm.F90
+++ core/biogeophys/cable_cbm.F90
@@ -215,7 +215,7 @@ CONTAINS
          veg%b_plant(i), veg%c_plant(i))
          canopy%kplant(i) = veg%kmax(i) * BAI / veg%hc(i) * plc
             
-         print*,'Entry kplant: ',DBH,BAI,plc,canopy%kplant(i)
+         print*,'Entry kplant: ',casapool%cplant(i,2)/1000.0_r_2,AGB_pl,DBH,BAI,plc,canopy%kplant(i)
 
       END DO
       !write(logn,*),'psi_rootzone mp1: ',ssnow%psi_rootzone(1)
