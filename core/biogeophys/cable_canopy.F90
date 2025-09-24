647322083fd56af59a844ac2ff8cae4911307196
Zihan Lu <zihan.lu@inrae.fr>
2025-09-24 11:16:40 +0200

    append wb and total_est_evap to iteration.txt

--- core/biogeophys/cable_canopy.F90
+++ core/biogeophys/cable_canopy.F90
@@ -3196,7 +3196,8 @@ CONTAINS
                   vcmxt3(i,1),vcmxt3(i,2),gs_coeff(i,1),gs_coeff(i,2),rdx(i,1),rdx(i,2),ex(i,1),ex(i,2), &
                   ssnow%rootR(i,1),ssnow%rootR(i,2),ssnow%rootR(i,3),ssnow%rootR(i,4),ssnow%rootR(i,5),ssnow%rootR(i,6), &
                   ssnow%soilR(i,1),ssnow%soilR(i,2),ssnow%soilR(i,3),ssnow%soilR(i,4),ssnow%soilR(i,5),ssnow%soilR(i,6), &
-                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i)
+                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i), &
+                  ssnow%wb(i,1),ssnow%wb(i,2),ssnow%wb(i,3),ssnow%wb(i,4),ssnow%wb(i,5),ssnow%wb(i,6),total_est_evap(i)
                elseif (present(wbpsdo) .and. present(fwpsdo)) then
                elseif (present (wbpsdo)) then 
                   write(135,*) ktau_tot, iter, i, k, tlfxx(i),tlfx(i),tlfxm(i), deltlf(i), &
@@ -3210,7 +3211,8 @@ CONTAINS
                   vcmxt3(i,1),vcmxt3(i,2),gs_coeff(i,1),gs_coeff(i,2),rdx(i,1),rdx(i,2),ex(i,1),ex(i,2), &
                   ssnow%rootR(i,1),ssnow%rootR(i,2),ssnow%rootR(i,3),ssnow%rootR(i,4),ssnow%rootR(i,5),ssnow%rootR(i,6), &
                   ssnow%soilR(i,1),ssnow%soilR(i,2),ssnow%soilR(i,3),ssnow%soilR(i,4),ssnow%soilR(i,5),ssnow%soilR(i,6), &
-                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i)
+                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i), &
+                  ssnow%wb(i,1),ssnow%wb(i,2),ssnow%wb(i,3),ssnow%wb(i,4),ssnow%wb(i,5),ssnow%wb(i,6),total_est_evap(i)
                elseif (present (vpdpsdo)) then 
                   write(136,*) ktau_tot, iter, i, k, tlfxx(i),tlfx(i),tlfxm(i), deltlf(i), &
                   dsxx(i),dsx(i),dsxm(i),abs_deltds(i),Numtag, Mtag, &
@@ -3223,7 +3225,8 @@ CONTAINS
                   vcmxt3(i,1),vcmxt3(i,2),gs_coeff(i,1),gs_coeff(i,2),rdx(i,1),rdx(i,2),ex(i,1),ex(i,2), &
                   ssnow%rootR(i,1),ssnow%rootR(i,2),ssnow%rootR(i,3),ssnow%rootR(i,4),ssnow%rootR(i,5),ssnow%rootR(i,6), &
                   ssnow%soilR(i,1),ssnow%soilR(i,2),ssnow%soilR(i,3),ssnow%soilR(i,4),ssnow%soilR(i,5),ssnow%soilR(i,6), &
-                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i)
+                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i), &
+                  ssnow%wb(i,1),ssnow%wb(i,2),ssnow%wb(i,3),ssnow%wb(i,4),ssnow%wb(i,5),ssnow%wb(i,6),total_est_evap(i)
                else
                   write(134,*) ktau_tot, iter, i, k, tlfxx(i),tlfx(i),tlfxm(i), deltlf(i), &
                   dsxx(i),dsx(i),dsxm(i),abs_deltds(i), Numtag,Mtag, &
@@ -3236,7 +3239,8 @@ CONTAINS
                   vcmxt3(i,1),vcmxt3(i,2),gs_coeff(i,1),gs_coeff(i,2),rdx(i,1),rdx(i,2),ex(i,1),ex(i,2), &
                   ssnow%rootR(i,1),ssnow%rootR(i,2),ssnow%rootR(i,3),ssnow%rootR(i,4),ssnow%rootR(i,5),ssnow%rootR(i,6), &
                   ssnow%soilR(i,1),ssnow%soilR(i,2),ssnow%soilR(i,3),ssnow%soilR(i,4),ssnow%soilR(i,5),ssnow%soilR(i,6), &
-                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i)
+                  kplantx(i),g0xx(i,1),g0xx(i,2),fwsoil(i),rad%rniso(i,1),rad%rniso(i,2),met%tk(i),met%dva(i), &
+                  ssnow%wb(i,1),ssnow%wb(i,2),ssnow%wb(i,3),ssnow%wb(i,4),ssnow%wb(i,5),ssnow%wb(i,6),total_est_evap(i)
 
                endif
 
