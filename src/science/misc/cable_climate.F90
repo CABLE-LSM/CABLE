6a0372de7f96fcce518199507f54e07d4b3a6e0f
Srbinovsky, Jhan (O&A, Aspendale) <Jhan.Srbinovsky@csiro.au>
2025-10-13 13:02:18 +1100

    included as IDK how you get around this with the library

--- src/science/misc/cable_climate.F90
+++ src/science/misc/cable_climate.F90
@@ -1,3 +1,4 @@
+#define UM_CBL YES
 !==============================================================================
 ! This source code is part of the
 ! Australian Community Atmosphere Biosphere Land Exchange (CABLE) model.
