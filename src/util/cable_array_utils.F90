85c6e97ac8d2b2af7e36fe971a480ca54c4b578a
Sean Bryan <sean.bryan@anu.edu.au>
2025-10-13 12:38:08 +1100

    src/util/cable_array_utils.F90: add array_partition subroutine

--- src/util/cable_array_utils.F90
+++ src/util/cable_array_utils.F90
@@ -25,4 +25,30 @@ contains
     end do
   end subroutine
 
+  subroutine array_partition(n, k, p, start, count)
+    !* Compute start and count for the p'th partition of an array of size n
+    ! where p = 0, 1, ... , k - 1.
+    !
+    ! For k partitions, an array of n elements can be partitioned into r
+    ! partitions of length q + 1, and k - r partitions of length q where q and r
+    ! are the quotient and remainder of n divided by k (i.e. n = q * k + r).
+    ! Note, we assume that the r partitions of length q + 1 precede the k - r
+    ! partitions of length q in the array.
+    integer, intent(in) :: n, k, p
+    integer, intent(out) :: start, count
+    integer :: q, r
+
+    q = n / k
+    r = mod(n, k)
+
+    if (p < r) then
+      count = q + 1
+      start = 1 + (q + 1) * p
+    else
+      count = q
+      start = 1 + (q + 1) * r + q * (p - r)
+    end if
+
+  end subroutine array_partition
+
 end module cable_array_utils_mod
