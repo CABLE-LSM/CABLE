39816f2c8e23ea8ffcaa2a8d60217b93639c812c
Sean Bryan <sean.bryan@anu.edu.au>
2026-03-06 15:46:02 +1100

    Add unit tests for parallel I/O netCDF interface
    GCC was updated from 13.x to 14.x to allow for passing a factory
    procedure with a polymorphic function result to each test case
    subroutine.
    
    See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=118372 for more
    details.

--- build.bash
+++ build.bash
@@ -116,7 +116,15 @@ if hostname -f | grep gadi.nci.org.au > /dev/null; then
             [[ -n ${mpi} ]] && module add intel-mpi/2019.5.281
             ;;
         gnu)
-            module add gcc/13.2.0
+            if [ $do_tests -eq 1 ]; then
+                # This is required to allow for passing a factory procedure
+                # with a polymorphic function result to various test cases. See
+                # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=118372 for more
+                # details.
+                module add gcc/14.1.0
+            else
+                module add gcc/13.2.0
+            fi
             compiler_lib_install_dir=GNU
             [[ -n ${mpi} ]] && module add openmpi/4.1.4
             ;;
