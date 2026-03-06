167cf634296a4148499f2ebfa49810eb31a6bc29
Sean Bryan <sean.bryan@anu.edu.au>
2026-03-06 15:50:01 +1100

    src/util/cable_array_utils.F90: Add array partition util

--- src/util/cable_array_utils.F90
+++ src/util/cable_array_utils.F90
@@ -9,6 +9,7 @@ module cable_array_utils_mod
 
   public array_offset
   public array_index
+  public array_partition
 
 contains
 
@@ -37,5 +38,34 @@ contains
       offset = (offset - 1) / shape(i) + 1
     end do
   end subroutine
+
+  subroutine array_partition(n, k, p, start, count)
+    !* Compute start and count for the `p`'th partition of an array of size `n`
+    ! where `p = 0, 1, ... , k - 1`.
+    !
+    ! For `k` partitions, an array of `n` elements can be partitioned into `r`
+    ! partitions of length `q + 1`, and `k - r` partitions of length `q` where `q`
+    ! and `r` are the quotient and remainder of `n` divided by `k` (i.e. `n = q *
+    ! k + r`).  Note, we assume that the `r` partitions of length `q + 1` precede
+    ! the `k - r` partitions of length `q` in the array.
+    integer, intent(in) :: n !! The total number of elements in the array to be partitioned.
+    integer, intent(in) :: k !! The total number of partitions.
+    integer, intent(in) :: p !! The index of the partition for which to compute the start and count (0-based).
+    integer, intent(out) :: start !! The starting index (1-based) of the `p`'th partition in the array.
+    integer, intent(out) :: count !! The number of elements in the `p`'th partition.
+    integer :: q, r
+
+    q = n / k
+    r = mod(n, k)
+
+    if (p < r) then
+      count = q + 1
+      start = 1 + (q + 1) * p
+    else
+      count = q
+      start = 1 + (q + 1) * r + q * (p - r)
+    end if
+
+  end subroutine array_partition
 
 end module cable_array_utils_mod
