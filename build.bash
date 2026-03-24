b3dac6aa9f8b539790dbd9bfd4f6015edba4d85b
Sean Bryan <sean.bryan@anu.edu.au>
2026-03-24 11:13:46 +1100

    squash! Add unit tests for parallel I/O netCDF interface

--- build.bash
+++ build.bash
@@ -116,10 +116,6 @@ if hostname -f | grep gadi.nci.org.au > /dev/null; then
             [[ -n ${mpi} ]] && module add intel-mpi/2019.5.281
             ;;
         gnu)
-            # This is required to allow for passing a factory procedure
-            # with a polymorphic function result to various test cases. See
-            # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=118372 for more
-            # details.
             module add gcc/14.1.0
             compiler_lib_install_dir=GNU
             [[ -n ${mpi} ]] && module add openmpi/4.1.4
