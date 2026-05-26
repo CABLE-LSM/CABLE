26c5d595b58dd31c658d0496b6d679d09e56911c
Lachlan Whyborn <lachlan.whyborn@anu.edu.au>
2026-05-26 16:54:37 +1000

    Add stash mapping

--- src/coupled/AM3/control/cable/shared/cable_surface_types_mod.F90
+++ src/coupled/AM3/control/cable/shared/cable_surface_types_mod.F90
@@ -6,14 +6,14 @@ MODULE cable_surface_types_mod
 
   ! cable_surface_type (nml) Index
   INTEGER, PARAMETER :: evergreen_needleleaf = 1
-  INTEGER, PARAMETER :: evergreen_broadleaf  = 2 
+  INTEGER, PARAMETER :: evergreen_broadleaf  = 2
   INTEGER, PARAMETER :: deciduous_needleleaf = 3
-  INTEGER, PARAMETER :: deciduous_broadleaf  = 4   
+  INTEGER, PARAMETER :: deciduous_broadleaf  = 4
   INTEGER, PARAMETER :: shrub_cable          = 5
-  INTEGER, PARAMETER :: c3_grassland         = 6 
-  INTEGER, PARAMETER :: c4_grassland         = 7 
+  INTEGER, PARAMETER :: c3_grassland         = 6
+  INTEGER, PARAMETER :: c4_grassland         = 7
   INTEGER, PARAMETER :: tundra               = 8
-  INTEGER, PARAMETER :: c3_cropland          = 9  
+  INTEGER, PARAMETER :: c3_cropland          = 9 
   INTEGER, PARAMETER :: c4_cropland          = 10 
   INTEGER, PARAMETER :: wetland              = 11 
   INTEGER, PARAMETER :: aust_mesic           = 12
@@ -25,10 +25,29 @@ MODULE cable_surface_types_mod
 
 CONTAINS
 
-  SUBROUTINE set_JULES_surface_types(nnpft, npft, nnvg, ntype, urban, lake,&
-      soil, ice)
+  SUBROUTINE set_JULES_surface_types(surface_type_ids, nnpft, npft, nnvg,&
+      ntype, urban, lake, soil, ice)
+    INTEGER, INTENT(INOUT) :: surface_type_ids
     INTEGER, INTENT(OUT) :: nnpft, npft, nnvp, ntype, urban, lake,&
       soil, ice
+
+    surface_type_ids(evergreen_needleleaf) = 1
+    surface_type_ids(evergreen_broadleaf ) = 2
+    surface_type_ids(deciduous_needleleaf) = 3
+    surface_type_ids(deciduous_broadleaf ) = 4
+    surface_type_ids(shrub_cable         ) = 5
+    surface_type_ids(c3_grassland        ) = 6
+    surface_type_ids(c4_grassland        ) = 7
+    surface_type_ids(tundra              ) = 8
+    surface_type_ids(c3_cropland         ) = 9
+    surface_type_ids(c4_cropland         ) = 10
+    surface_type_ids(wetland             ) = 11
+    surface_type_ids(aust_mesic          ) = 12
+    surface_type_ids(aust_xeric          ) = 13
+    surface_type_ids(barren_cable        ) = 14
+    surface_type_ids(urban_cable         ) = 15
+    surface_type_ids(lakes_cable         ) = 16
+    surface_type_ids(ice_cable           ) = 17
 
     npft = 13
     nnvg = 4
